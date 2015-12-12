0471e8e7b9ef2f25b3c7cc8a82b0079d9292db5f
avdgrinten <alexander.vandergrinten@gmail.com>
2015-12-12 21:35:12 +0100

    Fix missing time.h stubs

--- libc/generic/ansi/src/time-stubs.cpp
+++ libc/generic/ansi/src/time-stubs.cpp
@@ -32,7 +32,14 @@ char *ctime(const time_t *timer){
 	__ensure(!"Not implemented");
 	__builtin_unreachable();
 }
-
+struct tm *gmtime(const time_t *timer) {
+	__ensure(!"Not implemented");
+	__builtin_unreachable();
+}
+struct tm *localtime(const time_t *timer) {
+	__ensure(!"Not implemented");
+	__builtin_unreachable();
+}
 size_t strftime(char *__restrict dest, size_t max_size,
 		const char *__restrict format, const struct tm *__restrict ptr) {
 	__ensure(!"Not implemented");
