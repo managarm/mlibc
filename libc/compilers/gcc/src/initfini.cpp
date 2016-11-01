005e9f5261dea1efc8bb4f78cefea302e26e1e0d
avdgrinten <alexander.vandergrinten@gmail.com>
2016-11-01 16:47:59 +0100

    Add hidden __cxa_guard_{acquire,release}()

--- libc/compilers/gcc/src/initfini.cpp
+++ libc/compilers/gcc/src/initfini.cpp
@@ -1,4 +1,9 @@
 
+extern "C" [[ gnu::visibility("hidden") ]] void __cxa_guard_acquire() {
+}
+extern "C" [[ gnu::visibility("hidden") ]] void __cxa_guard_release() {
+}
+
 extern "C" [[ gnu::visibility("hidden") ]] void __mlibc_do_ctors() {
 
 }
