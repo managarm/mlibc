e9d4dbc0fec0107336bc626560094abf143751fa
Alexander van der Grinten <alexander.vandergrinten@gmail.com>
2017-12-28 13:35:27 +0100

    libc: Add no-op stubs for getopt()

--- libc/generic/linux/src/getopt-stubs.cpp
+++ libc/generic/linux/src/getopt-stubs.cpp
@@ -1,14 +1,16 @@
 
 #include <getopt.h>
+
+#include <frigg/debug.hpp>
 #include <mlibc/ensure.h>
 
 int getopt_long(int, char *const[], const char *, const struct option *, int *) {
-	__ensure(!"Not implemented");
-	__builtin_unreachable();
+	frigg::infoLogger() << "\e[31mmlibc: getopt_long() is only a stub\e[39m" << frigg::endLog;
+	return -1;
 }
 
 int getopt_long_only(int, char *const[], const char *, const struct option *, int *) {
-	__ensure(!"Not implemented");
-	__builtin_unreachable();
+	frigg::infoLogger() << "\e[31mmlibc: getopt_long_only() is only a stub\e[39m" << frigg::endLog;
+	return -1;
 }
 
