72d5bb76739ed4d60f4ee27da2f6a8a64006f033
Alexander van der Grinten <alexander.vandergrinten@gmail.com>
2018-02-13 20:24:52 +0100

    options/posix: Add reverse lookups to dlapi

--- options/posix/generic/dlfcn-stubs.cpp
+++ options/posix/generic/dlfcn-stubs.cpp
@@ -4,9 +4,17 @@
 
 #include <frigg/debug.hpp>
 
+struct __dlapi_symbol {
+	const char *file;
+	void *base;
+	const char *symbol;
+	void *address;
+};
+
 extern "C" const char *__dlapi_error();
 extern "C" void *__dlapi_open(const char *, int);
 extern "C" void *__dlapi_resolve(void *, const char *);
+extern "C" int __dlapi_reverse(const void *, __dlapi_symbol *);
 
 int dlclose(void *) {
 	frigg::infoLogger() << "\e[31mmlibc: dlclose() is a no-op\e[39m" << frigg::endLog;
@@ -32,9 +40,16 @@ void *dlsym(void *__restrict handle, const char *__restrict string) {
 }
 
 //gnu extensions
-int dladdr(const void *, Dl_info *) {
-	__ensure(!"dladdr() not implemented");
-	__builtin_unreachable();
+int dladdr(const void *ptr, Dl_info *out) {
+	__dlapi_symbol info;
+	if(__dlapi_reverse(ptr, &info))
+		return 0;
+
+	out->dli_fname = info.file;
+	out->dli_fbase = info.base;
+	out->dli_sname = info.symbol;
+	out->dli_saddr = info.address;
+	return 1;
 }
 
 int dlinfo(void *, int, void *) {
