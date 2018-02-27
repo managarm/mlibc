0e2293e8fce2069cbe3ada10f7965fe827fc1463
Alexander van der Grinten <alexander.vandergrinten@gmail.com>
2018-02-27 19:50:04 +0100

    options/posix: Implement basename()

--- options/posix/generic/libgen-stubs.cpp
+++ options/posix/generic/libgen-stubs.cpp
@@ -1,10 +1,24 @@
 
-#include <libgen.h>
 #include <bits/ensure.h>
+#include <libgen.h>
+#include <string.h>
 
-char *basename(char *) {
-	__ensure(!"Not implemented");
-	__builtin_unreachable();
+// Adopted from musl's code.
+char *basename(char *s) {
+	// This empty string behavior is specified by POSIX.
+	if (!s || !*s)
+		return const_cast<char *>(".");
+	
+	// Delete trailing slashes.
+	// Note that we do not delete the slash at index zero.
+	auto i = strlen(s) - 1;
+	for(; i && s[i] == '/'; i--)
+		s[i] = 0;
+
+	// Find the last non-trailing slash.
+	for(; i && s[i - 1] != '/'; i--)
+		;
+	return s + i;
 }
 
 char *dirname(char *) {
