661ece24c91f9efa2844946d4efa1de6f90fa7d7
Alexander van der Grinten <alexander.vandergrinten@gmail.com>
2018-11-08 20:11:17 +0100

    managarm: Clean up sysdeps support file

--- sysdeps/managarm/rtdl-generic/support.cpp
+++ sysdeps/managarm/rtdl-generic/support.cpp
@@ -37,28 +37,6 @@ size_t strlen(const char *str) {
 	return length;
 }
 
-void friggBeginLog() { }
-void friggEndLog() { }
-
-void friggPrintCritical(char c) {
-	helLog(&c, 1);
-}
-
-void friggPrintCritical(const char *str) {
-	int len = 0;
-	for(int i = 0; str[i]; i++)
-		len++;
-	helLog(str, len);
-}
-
-void friggPanic() {
-	const char *str = "Panic in mlibc";
-	int len = 0;
-	for(int i = 0; str[i]; i++)
-		len++;
-	helPanic(str, len);
-}
-
 extern "C" void frg_panic(const char *mstr) {
 	int mlen = 0;
 	while(mstr[mlen])
