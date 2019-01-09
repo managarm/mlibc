e02e1f4cc7009a6ea798fc5e42336bbb8d384bb7
Alexander van der Grinten <alexander.vandergrinten@gmail.com>
2019-01-09 20:06:52 +0100

    options/posix: Improve static uname information

--- options/posix/generic/sys-utsname-stubs.cpp
+++ options/posix/generic/sys-utsname-stubs.cpp
@@ -4,14 +4,15 @@
 
 #include <bits/ensure.h>
 #include <mlibc/debug.hpp>
+#include <internal-config.h>
 
 int uname(struct utsname *p) {
 	__ensure(p);
 	mlibc::infoLogger() << "\e[31mmlibc: uname() returns static information\e[39m" << frg::endlog;
-	strcpy(p->sysname, "managarm");
-	strcpy(p->nodename, "foo");
-	strcpy(p->release, "0.1.0");
-	strcpy(p->version, "0.1.0 vanilla");
+	strcpy(p->sysname, MLIBC_SYSTEM_NAME);
+	strcpy(p->nodename, "?");
+	strcpy(p->release, "?");
+	strcpy(p->version, "?");
 	strcpy(p->machine, "x86_64");
 	return 0;
 }
