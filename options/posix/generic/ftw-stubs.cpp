9139568da8d61522fd72e87620d69d356a171383
Alexander van der Grinten <alexander.vandergrinten@gmail.com>
2019-02-24 11:13:40 +0100

    options/posix: Add stub for ftw()

--- options/posix/generic/ftw-stubs.cpp
+++ options/posix/generic/ftw-stubs.cpp
@@ -3,6 +3,11 @@
 
 #include <bits/ensure.h>
 
+int ftw(const char *path, int (*fn)(const char *, const struct stat *, int), int fd_limit) {
+	__ensure(!"ftw() not implemented");
+	__builtin_unreachable();
+}
+
 int nftw(const char *path, int (*fn)(const char *, const struct stat *, int, struct FTW *),
 		int fd_limit, int flags) {
 	__ensure(!"nftw() not implemented");
--- options/posix/include/ftw.h
+++ options/posix/include/ftw.h
@@ -4,18 +4,18 @@
 
 #include <bits/posix/stat.h>
 
-#define FTW_F 1;
-#define FTW_D 2;
-#define FTW_DNR 3;
-#define FTW_DP 4;
-#define FTW_NS 5;
-#define FTW_SL 6;
-#define FTW_SLN 7;
+#define FTW_F 1
+#define FTW_D 2
+#define FTW_DNR 3
+#define FTW_DP 4
+#define FTW_NS 5
+#define FTW_SL 6
+#define FTW_SLN 7
 
-#define FTW_PHYS 1;
-#define FTW_MOUNT 2;
-#define FTW_DEPTH 4;
-#define FTW_CHDIR 8;
+#define FTW_PHYS 1
+#define FTW_MOUNT 2
+#define FTW_DEPTH 4
+#define FTW_CHDIR 8
 
 #ifdef __cplusplus
 extern "C" {
@@ -26,6 +26,7 @@ struct FTW {
 	int level;
 };
 
+int ftw(const char *, int (*)(const char *, const struct stat *, int), int);
 int nftw(const char *, int (*)(const char *, const struct stat *, int, struct FTW *), int, int);
 
 #ifdef __cplusplus
