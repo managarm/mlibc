67876e21e3cd239b7406700a57f1f98ccea9393a
Alexander van der Grinten <alexander.vandergrinten@gmail.com>
2019-02-24 11:13:59 +0100

    options/posix: Add endianness #defs in sys/param.h

--- options/posix/include/sys/param.h
+++ options/posix/include/sys/param.h
@@ -2,6 +2,15 @@
 #ifndef _SYS_PARAM_H
 #define _SYS_PARAM_H
 
+#ifdef __GNUC__
+# 	define BYTE_ORDER __BYTE_ORDER__
+#	define LITTLE_ENDIAN __ORDER_LITTLE_ENDIAN__
+#	define BIG_ENDIAN __ORDER_BIG_ENDIAN__
+#	define PDP_ENDIAN __ORDER_PDP_ENDIAN__
+#else
+#	error "Unsupported compiler"
+#endif
+
 #ifdef __cplusplus
 extern "C" {
 #endif
