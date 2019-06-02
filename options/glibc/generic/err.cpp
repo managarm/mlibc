7beafe1a210ca3ba52637f586197d3b5e74df4d9
ArsenArsen <arsenarsentmc@outlook.com>
2019-06-02 20:50:13 +0200

    options/glibc: implement err(3) BSD extensions

---
+++ options/glibc/generic/err.cpp
@@ -0,0 +1,64 @@
+#include <err.h>
+
+#include <errno.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+// va_list
+
+void vwarn(const char *fmt, va_list params) {
+	fprintf(stderr, "%s: ", program_invocation_short_name);
+	if (fmt) {
+		vfprintf(stderr, fmt, params);
+                fwrite(": ", 1, 2, stderr);
+	}
+	perror(NULL);
+}
+
+void vwarnx(const char *fmt, va_list params) {
+	fprintf(stderr, "%s: ", program_invocation_short_name);
+	if (fmt) {
+		vfprintf(stderr, fmt, params);
+	}
+	putc('\n', stderr);
+}
+
+__attribute__((noreturn)) void verr(int status, const char *fmt, va_list params) {
+	vwarn(fmt, params);
+	exit(status);
+}
+
+__attribute__((noreturn)) void verrx(int status, const char *fmt, va_list params) {
+	vwarnx(fmt, params);
+	exit(status);
+}
+
+// variadic
+
+void warn(const char *fmt, ...) {
+	va_list params;
+	va_start(params, fmt);
+	vwarn(fmt, params);
+	va_end(params);
+}
+
+void warnx(const char *fmt, ...) {
+	va_list params;
+	va_start(params, fmt);
+	vwarnx(fmt, params);
+	va_end(params);
+}
+
+__attribute__((noreturn)) void err(int status, const char *fmt, ...) {
+	va_list params;
+	va_start(params, fmt);
+	verr(status, fmt, params);
+	va_end(params);
+}
+
+__attribute__((noreturn)) void errx(int status, const char *fmt, ...) {
+	va_list params;
+	va_start(params, fmt);
+	verrx(status, fmt, params);
+	va_end(params);
+}
