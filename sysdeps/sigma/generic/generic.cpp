2e2a0489ab90ecfac41c77c53b0d1d91432f3b67
Thomas Woertman <twoertman@gmail.com>
2019-08-19 18:56:25 +0200

    Comment out most stubbed functions

--- sysdeps/sigma/generic/generic.cpp
+++ sysdeps/sigma/generic/generic.cpp
@@ -12,24 +12,23 @@
 
 namespace mlibc {
     // Unknown Functions
-    int sys_sigaction(int signum, const struct sigaction *act, struct sigaction *oldact) STUB_ONLY
+    //int sys_sigaction(int signum, const struct sigaction *act, struct sigaction *oldact) STUB_ONLY
     
     // Memory Related Functions
     int sys_vm_map(void *hint, size_t size, int prot, int flags, int fd, off_t offset, void **window) STUB_ONLY
-    #ifndef MLIBC_BUILDING_RTDL
+    /*#ifndef MLIBC_BUILDING_RTDL
     int sys_vm_remap(void *pointer, size_t size, size_t new_size, void **window) STUB_ONLY
-    #endif
+    #endif*/
     int sys_vm_unmap(void *pointer, size_t size) STUB_ONLY
 
     // Misc functions
     #ifndef MLIBC_BUILDING_RTDL
     int sys_clock_get(int clock, time_t *secs, long *nanos) STUB_ONLY
+
     int sys_futex_wait(int *pointer, int expected) STUB_ONLY
     int sys_futex_wake(int *pointer) STUB_ONLY
-    #endif
 
     // Task functions
-    #ifndef MLIBC_BUILDING_RTDL
     void sys_exit(int status){
         (void)(status);
         libsigma_kill();
@@ -39,7 +38,11 @@ namespace mlibc {
         libsigma_set_fsbase(reinterpret_cast<uint64_t>(pointer));
         return 0;
     }
+    #endif
 
+
+
+    /*#ifndef MLIBC_BUILDING_RTDL
     int sys_sleep(time_t *secs, long *nanos) STUB_ONLY
     int sys_fork(pid_t *child) STUB_ONLY
     int sys_execve(const char *path, char *const argv[], char *const envp[]) STUB_ONLY
@@ -54,7 +57,7 @@ namespace mlibc {
     uid_t sys_geteuid() STUB_ONLY
     pid_t sys_getpid() STUB_ONLY
     pid_t sys_getppid() STUB_ONLY
-    #endif
+    #endif*/
 
     // File functions
     int sys_open(const char *path, int flags, int *fd) STUB_ONLY
@@ -68,6 +71,9 @@ namespace mlibc {
 
     #ifndef MLIBC_BUILDING_RTDL
     int sys_write(int fd, const void *buf, size_t count, ssize_t *bytes_written) STUB_ONLY
+    #endif
+
+    /*
     int sys_ioctl(int fd, unsigned long request, void *arg, int *result) STUB_ONLY
     int sys_stat(fsfd_target fsfdt, int fd, const char *path, int flags, struct stat *statbuf) STUB_ONLY
     int sys_rename(const char *path, const char *new_path) STUB_ONLY
@@ -104,7 +110,5 @@ namespace mlibc {
     int sys_sockname(int fd, struct sockaddr *addr_ptr, socklen_t max_addr_length, socklen_t *actual_length) STUB_ONLY
     int sys_getsockopt(int fd, int layer, int number, void *__restrict buffer, socklen_t *__restrict size) STUB_ONLY
     int sys_setsockopt(int fd, int layer, int number, const void *buffer, socklen_t size) STUB_ONLY
-    #endif
-
-
+    #endif*/
 }
