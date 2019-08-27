1ed541cdf5f2a455ecca194b9c370e7cf05b238d
mintsuki <mintsuki@protonmail.com>
2019-08-27 16:52:15 +0200

    sys/socket.h: Add sockaddr_storage struct

--- options/posix/include/sys/socket.h
+++ options/posix/include/sys/socket.h
@@ -20,7 +20,10 @@ struct sockaddr {
 	char sa_data[];
 };
 
-//MISSING: struct sockaddr_storage
+struct sockaddr_storage {
+	sa_family_t ss_family;
+	char __padding[128 - sizeof(sa_family_t)];
+};
 
 struct msghdr {
 	void *msg_name;
