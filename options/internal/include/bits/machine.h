8bf657cf76c9166795038fd7f49e753429250b0e
mintsuki <mintsuki@protonmail.com>
2019-09-26 07:56:32 +0200

    Begin work to port mlibc to i386

--- options/internal/include/bits/machine.h
+++ options/internal/include/bits/machine.h
@@ -2,7 +2,13 @@
 #ifndef MLIBC_MACHINE_H
 #define MLIBC_MACHINE_H
 
-#define __MLIBC_JMPBUF_SIZE 8
+#if defined (__i386__)
+#  define __MLIBC_JMPBUF_SIZE 6
+#elif defined (__x86_64__)
+#  define __MLIBC_JMPBUF_SIZE 8
+#else
+#  error "Missing architecture specific code"
+#endif
 
 #endif // MLIBC_MACHINE_H
 
