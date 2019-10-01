cf9acb8ee627ff2f8ab25141b8898655ed85ddeb
mintsuki <mintsuki@protonmail.com>
2019-10-01 09:17:18 +0200

    elf/startup: Commenting run_constructors out for now

--- options/elf/generic/startup.cpp
+++ options/elf/generic/startup.cpp
@@ -20,6 +20,7 @@ struct global_constructor_guard {
 static global_constructor_guard g;
 
 void __mlibc_run_constructors() {
+/*
     if (!constructors_ran_already) {
         size_t constructor_count = (size_t)__init_array_end - (size_t)__init_array_start;
         constructor_count /= sizeof(void*);
@@ -28,6 +29,7 @@ void __mlibc_run_constructors() {
             ptr();
         }
     }
+*/
 }
 
 namespace mlibc {
