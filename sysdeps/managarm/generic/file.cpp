09c19b46555cb64e2c6c55c3799cc64ef0f669df
Alexander van der Grinten <alexander.vandergrinten@gmail.com>
2019-12-14 21:34:58 +0100

    sysdeps/managarm: Implement sys_vm_protect()

--- sysdeps/managarm/generic/file.cpp
+++ sysdeps/managarm/generic/file.cpp
@@ -701,6 +701,45 @@ int sys_vm_remap(void *pointer, size_t size, size_t new_size, void **window) {
 	return 0;
 }
 
+int sys_vm_protect(void *pointer, size_t size, int prot) {
+	SignalGuard sguard;
+	HelAction actions[3];
+	globalQueue.trim();
+
+	managarm::posix::CntRequest<MemoryAllocator> req(getSysdepsAllocator());
+	req.set_request_type(managarm::posix::CntReqType::VM_PROTECT);
+	req.set_address(reinterpret_cast<uintptr_t>(pointer));
+	req.set_size(size);
+	req.set_mode(prot);
+
+	frg::string<MemoryAllocator> ser(getSysdepsAllocator());
+	req.SerializeToString(&ser);
+	actions[0].type = kHelActionOffer;
+	actions[0].flags = kHelItemAncillary;
+	actions[1].type = kHelActionSendFromBuffer;
+	actions[1].flags = kHelItemChain;
+	actions[1].buffer = ser.data();
+	actions[1].length = ser.size();
+	actions[2].type = kHelActionRecvInline;
+	actions[2].flags = 0;
+	HEL_CHECK(helSubmitAsync(getPosixLane(), actions, 3,
+			globalQueue.getQueue(), 0, 0));
+
+	auto element = globalQueue.dequeueSingle();
+	auto offer = parseSimple(element);
+	auto send_req = parseSimple(element);
+	auto recv_resp = parseInline(element);
+
+	HEL_CHECK(offer->error);
+	HEL_CHECK(send_req->error);
+	HEL_CHECK(recv_resp->error);
+
+	managarm::posix::SvrResponse<MemoryAllocator> resp(getSysdepsAllocator());
+	resp.ParseFromArray(recv_resp->data, recv_resp->length);
+	__ensure(resp.error() == managarm::posix::Errors::SUCCESS);
+	return 0;
+}
+
 int sys_vm_unmap(void *pointer, size_t size) {
 	SignalGuard sguard;
 	HelAction actions[3];
