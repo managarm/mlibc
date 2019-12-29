b377e3bdca89f988e6360756585a5850cda2a60d
mintsuki <mintsuki@protonmail.com>
2019-12-29 09:33:35 +0100

    qword: Move mount to own header

---
+++ sysdeps/qword/include/qword/mount.h
@@ -0,0 +1,16 @@
+#ifndef _QWORD_MOUNT_H
+#define _QWORD_MOUNT_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+int mount(const char *source, const char *target,
+    const char *filesystemtype, unsigned long mountflags,
+    const void *data);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif // _QWORD_MOUNT_H
