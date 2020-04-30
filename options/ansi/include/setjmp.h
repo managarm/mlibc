dafcb1547c2c0596ed96afae25f38770ee924336
Dennisbonke <admin@dennisbonke.com>
2020-04-30 14:17:46 +0200

    options/ansi: define sigsetjmp and siglongjmp. options/posix: define sched_param and stub more posix_spawnattr functions.
    Signed-off-by: Dennisbonke <admin@dennisbonke.com>

--- options/ansi/include/setjmp.h
+++ options/ansi/include/setjmp.h
@@ -3,6 +3,7 @@
 #define _SETJMP_H
 
 #include <bits/machine.h>
+#include <abi-bits/signal.h>
 
 #ifdef __cplusplus
 extern "C" {
@@ -13,8 +14,16 @@ extern "C" {
 // FIXME: use intptr_t equivalent
 typedef long jmp_buf[__MLIBC_JMPBUF_SIZE];
 
+typedef struct sigjmp_buf {
+	jmp_buf __jmpbuf;
+	int __mask_was_saved;
+	sigset_t __saved_mask;
+} sigjmp_buf;
+
 __attribute__ (( returns_twice )) int setjmp(jmp_buf buffer);
 __attribute__ (( noreturn )) void longjmp(jmp_buf buffer, int value);
+__attribute__ (( returns_twice )) int sigsetjmp(sigjmp_buf buffer, int savesigs);
+__attribute__ (( noreturn )) void siglongjmp(sigjmp_buf buffer, int value);
 
 #ifdef __cplusplus
 }
