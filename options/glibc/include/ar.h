494e91426c69e65ac5f5e0a1351f31e09d5873fe
Dennisbonke <admin@dennisbonke.com>
2020-05-20 09:46:33 +0200

    options/glibc: Added ar.h as it is required for make
    Signed-off-by: Dennisbonke <admin@dennisbonke.com>

---
+++ options/glibc/include/ar.h
@@ -0,0 +1,25 @@
+
+#ifndef  _AR_H
+#define  _AR_H
+
+#define ARMAG  "!<arch>\n"
+#define SARMAG 8
+#define ARFMAG "`\n"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+struct ar_hdr {
+	char ar_name[16];
+	char ar_date[12];
+	char ar_uid[6];
+	char ar_gid[6];
+	char ar_mode[8];
+	char ar_size[10];
+	char ar_fmag[2];
+};
+
+#endif
+
+#endif
