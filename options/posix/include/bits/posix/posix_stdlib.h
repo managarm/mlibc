88da8a3e8b085dc50f441632a71d0e0c0aeb38ac
Dennisbonke <admin@dennisbonke.com>
2020-06-18 16:57:59 +0200

    options/posix: Implement initstate, setstate, random and srandom, add _SC_CHILD_MAX in sysconf
    Signed-off-by: Dennisbonke <admin@dennisbonke.com>

--- options/posix/include/bits/posix/posix_stdlib.h
+++ options/posix/include/bits/posix/posix_stdlib.h
@@ -11,6 +11,7 @@ double drand48(void);
 void srand48(long int);
 char *initstate(unsigned int, char *, size_t);
 char *setstate(char *);
+void srandom(unsigned int);
 
 // ----------------------------------------------------------------------------
 // Environment.
