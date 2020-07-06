7d8a9090c7039cbd427e3a9505044691a13cf168
Kacper Słomiński <kacper.slominski72@gmail.com>
2020-07-06 17:46:25 +0200

    sysdeps/managarm: convert mount request to bragi

--- sysdeps/managarm/generic/mount.cpp
+++ sysdeps/managarm/generic/mount.cpp
@@ -6,46 +6,34 @@
 #include <mlibc/allocator.hpp>
 #include <mlibc/posix-pipe.hpp>
 #include <posix.frigg_bragi.hpp>
+#include <bragi/helpers-frigg.hpp>
 
 namespace mlibc {
 
 int sys_mount(const char *source, const char *target,
 		const char *fstype, unsigned long flags, const void *data) {
 	SignalGuard sguard;
-	HelAction actions[3];
 
-	globalQueue.trim();
-
-	managarm::posix::CntRequest<MemoryAllocator> req(getSysdepsAllocator());
-	req.set_request_type(managarm::posix::CntReqType::MOUNT);
+	managarm::posix::MountRequest<MemoryAllocator> req(getSysdepsAllocator());
 	req.set_path(frg::string<MemoryAllocator>(getSysdepsAllocator(), source));
 	req.set_target_path(frg::string<MemoryAllocator>(getSysdepsAllocator(), target));
 	req.set_fs_type(frg::string<MemoryAllocator>(getSysdepsAllocator(), fstype));
 
-	frg::string<MemoryAllocator> ser(getSysdepsAllocator());
-	req.SerializeToString(&ser);
-	actions[0].type = kHelActionOffer;
-	actions[0].flags = kHelItemAncillary;
-	actions[1].type = kHelActionSendFromBuffer;
-	actions[1].flags = kHelItemChain;
-	actions[1].buffer = ser.data();
-	actions[1].length = ser.size();
-	actions[2].type = kHelActionRecvInline;
-	actions[2].flags = 0;
-	HEL_CHECK(helSubmitAsync(getPosixLane(), actions, 3,
-			globalQueue.getQueue(), 0, 0));
+	auto [offer, send_head, send_tail, recv_resp] =
+		exchangeMsgsSync(
+			getPosixLane(),
+			helix_ng::offer(
+				helix_ng::sendBragiHeadTail(req, getSysdepsAllocator()),
+				helix_ng::recvInline()
+			)
+		);
 
-	auto element = globalQueue.dequeueSingle();
-	auto offer = parseSimple(element);
-	auto send_req = parseSimple(element);
-	auto recv_resp = parseInline(element);
+	HEL_CHECK(offer.error());
+	HEL_CHECK(send_head.error());
+	HEL_CHECK(send_tail.error());
+	HEL_CHECK(recv_resp.error());
 
-	HEL_CHECK(offer->error);
-	HEL_CHECK(send_req->error);
-	HEL_CHECK(recv_resp->error);
-
-	managarm::posix::SvrResponse<MemoryAllocator> resp(getSysdepsAllocator());
-	resp.ParseFromArray(recv_resp->data, recv_resp->length);
+	auto resp = *bragi::parse_head_only<managarm::posix::SvrResponse>(recv_resp, getSysdepsAllocator());
 	__ensure(resp.error() == managarm::posix::Errors::SUCCESS);
 	return 0;
 }
