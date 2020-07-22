af6637dbc1e695cf135bc6d4dcf7adc73e8d5118
BrainStackOverFlow <48990146+BrainStackOverFlow@users.noreply.github.com>
2020-07-22 14:57:49 +0300

    struct rusage fix

--- abis/mlibc/resource.h
+++ abis/mlibc/resource.h
@@ -6,6 +6,20 @@
 struct rusage {
 	struct timeval ru_utime;
 	struct timeval ru_stime;
+	long int ru_maxrss;
+	long int ru_ixrss;
+	long int ru_idrss;
+	long int ru_isrss;
+	long int ru_minflt;
+	long int ru_majflt;
+	long int ru_nswap;
+	long int ru_inblock;
+	long int ru_oublock;
+	long int ru_msgsnd;
+	long int ru_msgrcv;
+	long int ru_nsignals;
+	long int ru_nvcsw;
+	long int ru_nivcsw;
 };
 
 #endif // _ABIBITS_RESOURCE_H
