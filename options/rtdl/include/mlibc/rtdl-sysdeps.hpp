5c139d081fa6622e1fcf879565af41020e952441
Alexander van der Grinten <alexander.vandergrinten@gmail.com>
2020-08-29 19:24:22 +0200

    options/rtdl: Fix missing sys_vm_readahead() sysdeps

--- options/rtdl/include/mlibc/rtdl-sysdeps.hpp
+++ options/rtdl/include/mlibc/rtdl-sysdeps.hpp
@@ -5,6 +5,8 @@ namespace mlibc [[gnu::visibility("hidden")]] {
 
 int sys_tcb_set(void *pointer);
 
+[[gnu::weak]] int sys_vm_readahead(void *pointer, size_t size);
+
 } // namespace mlibc
 
 #endif // MLIBC_RTDL_SYSDEPS
