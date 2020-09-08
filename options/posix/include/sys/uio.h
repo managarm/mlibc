31844e3de7e12dacddec7e700a932ac9293e0db2
Dennisbonke <admin@dennisbonke.com>
2020-09-09 01:59:53 +0200

    options/posix: Stub preadv and pwritev
    Signed-off-by: Dennisbonke <admin@dennisbonke.com>

--- options/posix/include/sys/uio.h
+++ options/posix/include/sys/uio.h
@@ -3,14 +3,19 @@
 
 #include <bits/posix/iovec.h>
 #include <bits/ssize_t.h>
+#include <bits/off_t.h>
 #include <bits/size_t.h>
 
 #ifdef __cplusplus
 extern "C" {
 #endif
 
-ssize_t readv(int, const struct iovec *, int);
-ssize_t writev(int, const struct iovec *, int);
+ssize_t readv(int fd, const struct iovec *iov, int iovcnt);
+ssize_t writev(int fd, const struct iovec *iov, int iovcnt);
+
+// Non standard extensions, also found on modern BSD's
+ssize_t preadv(int fd, const struct iovec *iov, int iovcnt, off_t offset);
+ssize_t pwritev(int fd, const struct iovec *iov, int iovcnt, off_t offset);
 
 #ifdef __cplusplus
 }
