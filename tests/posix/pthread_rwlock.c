7e30208ac67cc42cec97384f320815bf9d5f8ac8
Alexander van der Grinten <alexander.vandergrinten@gmail.com>
2020-12-29 16:21:23 +0100

    tests: Add test for pthread rwlocks

---
+++ tests/posix/pthread_rwlock.c
@@ -0,0 +1,94 @@
+#include <assert.h>
+#include <errno.h>
+#include <pthread.h>
+
+static void test_write_lock_unlock() {
+	int res;
+	pthread_rwlock_t rw = PTHREAD_RWLOCK_INITIALIZER;
+	res = pthread_rwlock_wrlock(&rw);
+	assert(!res);
+	res = pthread_rwlock_unlock(&rw);
+	assert(!res);
+}
+
+static void test_read_lock_unlock() {
+	int res;
+	pthread_rwlock_t rw = PTHREAD_RWLOCK_INITIALIZER;
+	res = pthread_rwlock_rdlock(&rw);
+	assert(!res);
+	res = pthread_rwlock_unlock(&rw);
+	assert(!res);
+}
+
+static void test_write_trylock_unlock() {
+	int res;
+	pthread_rwlock_t rw = PTHREAD_RWLOCK_INITIALIZER;
+	res = pthread_rwlock_trywrlock(&rw);
+	assert(!res);
+	res = pthread_rwlock_unlock(&rw);
+	assert(!res);
+}
+
+static void test_read_trylock_unlock() {
+	int res;
+	pthread_rwlock_t rw = PTHREAD_RWLOCK_INITIALIZER;
+	res = pthread_rwlock_tryrdlock(&rw);
+	assert(!res);
+	res = pthread_rwlock_unlock(&rw);
+	assert(!res);
+}
+
+static void test_write_prevents_read() {
+	int res;
+	pthread_rwlock_t rw = PTHREAD_RWLOCK_INITIALIZER;
+	res = pthread_rwlock_wrlock(&rw);
+	assert(!res);
+	res = pthread_rwlock_tryrdlock(&rw);
+	assert(res == EBUSY);
+	res = pthread_rwlock_unlock(&rw);
+	assert(!res);
+}
+
+static void test_write_prevents_write() {
+	int res;
+	pthread_rwlock_t rw = PTHREAD_RWLOCK_INITIALIZER;
+	res = pthread_rwlock_wrlock(&rw);
+	assert(!res);
+	res = pthread_rwlock_trywrlock(&rw);
+	assert(res == EBUSY);
+	res = pthread_rwlock_unlock(&rw);
+	assert(!res);
+}
+
+static void test_read_prevents_write() {
+	int res;
+	pthread_rwlock_t rw = PTHREAD_RWLOCK_INITIALIZER;
+	res = pthread_rwlock_rdlock(&rw);
+	assert(!res);
+	res = pthread_rwlock_trywrlock(&rw);
+	assert(res == EBUSY);
+	res = pthread_rwlock_unlock(&rw);
+	assert(!res);
+}
+
+static void test_read_allows_read() {
+	int res;
+	pthread_rwlock_t rw = PTHREAD_RWLOCK_INITIALIZER;
+	res = pthread_rwlock_rdlock(&rw);
+	assert(!res);
+	res = pthread_rwlock_tryrdlock(&rw);
+	assert(!res);
+	res = pthread_rwlock_unlock(&rw);
+	assert(!res);
+}
+
+int main() {
+	test_write_lock_unlock();
+	test_read_lock_unlock();
+	test_write_trylock_unlock();
+	test_read_trylock_unlock();
+	test_write_prevents_read();
+	test_write_prevents_write();
+	test_read_prevents_write();
+	test_read_allows_read();
+}
