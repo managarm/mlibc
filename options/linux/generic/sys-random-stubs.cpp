1e359c22403bce4b12db58985b7c6b91776db98f
Dennisbonke <admin@dennisbonke.com>
2020-12-30 16:43:47 +0100

    options/linux: Implement getrandom
    Signed-off-by: Dennisbonke <admin@dennisbonke.com>

--- options/linux/generic/sys-random-stubs.cpp
+++ options/linux/generic/sys-random-stubs.cpp
@@ -3,9 +3,24 @@
 #include <bits/ensure.h>
 
 #include <mlibc/debug.hpp>
+#include <mlibc/posix-sysdeps.hpp>
+
+#include <errno.h>
 
 ssize_t getrandom(void *buffer, size_t max_size, unsigned int flags) {
-	mlibc::infoLogger() << "\e[31mmlibc: getrandom() is a no-op\e[39m" << frg::endlog;
+	if(flags & ~(GRND_RANDOM | GRND_NONBLOCK)) {
+		errno = EINVAL;
+		return -1;
+	}
+	if(!mlibc::sys_getentropy) {
+		MLIBC_MISSING_SYSDEP();
+		errno = ENOSYS;
+		return -1;
+	}
+	if(int e = mlibc::sys_getentropy(buffer, max_size); e) {
+		errno = e;
+		return -1;
+	}
 	return max_size;
 }
 
