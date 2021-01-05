67c37cf44aae06a661d3e59e8d2270854dfdfa62
Geert Custers <geert.aj.custers@gmail.com>
2021-01-05 18:21:30 +0100

    options/posix: don't close fd in vdprintf()

---
+++ tests/posix/dprintf.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <assert.h>
+#include <errno.h>
+
+#define TEST_FILE "dprintf.tmp"
+
+int main() {
+	int fd = open(TEST_FILE, O_RDWR | O_CREAT);
+	assert(fd > -1);
+
+	int ret = dprintf(fd, "aaa");
+	assert(ret == 3);
+
+	// Check if we can read back the same things.
+	// Also checks to see if the fd is still open,
+	// which is issue #199.
+
+	off_t seek = lseek(fd, 0, SEEK_SET);
+	assert(seek == 0);
+
+	char buf[3];
+	ssize_t num_read = read(fd, buf, 3);
+
+	assert(num_read == 3);
+	assert(buf[0] == 'a'
+		&& buf[1] == 'a'
+		&& buf[2] == 'a');
+
+	// All the tests pass, now we can unlink the file.
+	ret = unlink(TEST_FILE);
+	assert(ret == 0);
+	return 0;
+}
