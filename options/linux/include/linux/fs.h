3a993357c6cb02e1007b73744ca1c59a02fcb894
Dennis Bonke <admin@dennisbonke.com>
2021-01-23 00:09:03 +0100

    options/linux: Add several blkdev related defines
    Signed-off-by: Dennis Bonke <admin@dennisbonke.com>

--- options/linux/include/linux/fs.h
+++ options/linux/include/linux/fs.h
@@ -5,6 +5,10 @@
 #include <sys/ioctl.h>
 
 #define BLKRRPART _IO(0x12, 95)
+#define BLKIOMIN _IO(0x12, 120)
+#define BLKIOOPT _IO(0x12, 121)
+#define BLKALIGNOFF _IO(0x12, 122)
+#define BLKPBSZGET _IO(0x12, 123)
 
 #endif // _LINUX_FS_H
 
