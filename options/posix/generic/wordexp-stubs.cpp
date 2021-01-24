108626dd3c734bf70256447d1027c21c7d0e9ccc
no92 <no92.mail@gmail.com>
2021-01-24 22:42:52 +0100

    options/posix: stub out wordexp

---
+++ options/posix/generic/wordexp-stubs.cpp
@@ -0,0 +1,11 @@
+#include <bits/ensure.h>
+#include <wordexp.h>
+
+int wordexp(const char *s, wordexp_t *p, int flags) {
+	__ensure(!"Not implemented");
+	__builtin_unreachable();
+}
+void wordfree(wordexp_t *p) {
+	__ensure(!"Not implemented");
+	__builtin_unreachable();
+}
