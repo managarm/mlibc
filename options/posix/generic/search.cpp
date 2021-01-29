12270301ff0d769cd9debc91641a966fd766a176
Dennis Bonke <admin@dennisbonke.com>
2021-01-29 20:14:18 +0100

    options/posix: Stubbed twalk and tdestroy and added the VISIT enum
    Signed-off-by: Dennis Bonke <admin@dennisbonke.com>

--- options/posix/generic/search.cpp
+++ options/posix/generic/search.cpp
@@ -16,3 +16,13 @@ void *tdelete(const void *, void **, int(*compar)(const void *, const void *)) {
 	__ensure(!"Not implemented");
 	__builtin_unreachable();
 }
+
+void twalk(const void *, void (*action)(const void *, VISIT, int)) {
+	__ensure(!"Not implemented");
+	__builtin_unreachable();
+}
+
+void tdestroy(void *, void (*free_node)(void *)) {
+	__ensure(!"Not implemented");
+	__builtin_unreachable();
+}
--- options/posix/include/search.h
+++ options/posix/include/search.h
@@ -6,9 +6,18 @@
 extern "C" {
 #endif
 
+typedef enum {
+  preorder,
+  postorder,
+  endorder,
+  leaf
+} VISIT;
+
 void *tsearch(const void *, void **, int(*compar)(const void *, const void *));
 void *tfind(const void *, void *const *, int (*compar)(const void *, const void *));
 void *tdelete(const void *, void **, int(*compar)(const void *, const void *));
+void twalk(const void *, void (*action)(const void *, VISIT, int));
+void tdestroy(void *, void (*free_node)(void *));
 
 #ifdef __cplusplus
 }
