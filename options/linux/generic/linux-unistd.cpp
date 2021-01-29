77e695abd2858d5b7e0d53db0926f1a5d41ba8cf
Dennis Bonke <admin@dennisbonke.com>
2021-01-29 19:38:57 +0100

    options/linux: Stub vhangup
    Signed-off-by: Dennis Bonke <admin@dennisbonke.com>

--- options/linux/generic/linux-unistd.cpp
+++ options/linux/generic/linux-unistd.cpp
@@ -5,3 +5,8 @@ int dup3(int fd, int newfd, int flags) {
 	__ensure(!"Not implemented");
 	__builtin_unreachable();
 }
+
+int vhangup(void) {
+	__ensure(!"Not implemented");
+	__builtin_unreachable();
+}
