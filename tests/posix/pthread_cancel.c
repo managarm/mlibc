39a463d6e32f8fb2a1dae3feb7f3fbfed6a59b0a
Geert Custers <geert.aj.custers@gmail.com>
2021-02-04 09:17:35 +0100

    tests: add more pthread_cancel tests

--- tests/posix/pthread_cancel.c
+++ tests/posix/pthread_cancel.c
@@ -2,33 +2,88 @@
 #include <pthread.h>
 #include <unistd.h>
 
-_Atomic int ready = 0;
+_Atomic int worker_ready = 0;
+_Atomic int main_ready = 0;
 
-static void *worker(void *arg) {
+static void *worker1(void *arg) {
 	(void)arg;
 	assert(!pthread_setcancelstate(PTHREAD_CANCEL_ENABLE, NULL));
 	assert(!pthread_setcanceltype(PTHREAD_CANCEL_ASYNCHRONOUS, NULL));
 
-	ready = 1;
+	worker_ready = 1;
 
 	while (1) sleep(1);
 
 	return NULL;
 }
 
-int main() {
-	pthread_t thread;
-	int ret = pthread_create(&thread, NULL, &worker, NULL);
-	assert(!ret);
+static void *worker2(void *arg) {
+	(void) arg;
+	assert(!pthread_setcancelstate(PTHREAD_CANCEL_ENABLE, NULL));
 
-	while (!ready);
+	worker_ready = 1;
 
-	ret = pthread_cancel(thread);
-	assert(!ret);
+	while(!main_ready);
+
+	// Cancellation point - we should cancel right now
+	sleep(1);
+
+	assert(!"Expected to be cancelled!");
+	__builtin_unreachable();
+}
+
+static void *worker3(void *arg) {
+	(void) arg;
+	assert(!pthread_setcancelstate(PTHREAD_CANCEL_ENABLE, NULL));
+
+	worker_ready = 1;
+
+	while(!main_ready);
+
+	// Cancellation point - we should cancel right now
+	pthread_testcancel();
+
+	assert(!"Expected to be cancelled!");
+	__builtin_unreachable();
+}
+
+static void check_result(pthread_t thread) {
 
 	void *ret_val = NULL;
-	ret = pthread_join(thread, &ret_val);
+	int ret = pthread_join(thread, &ret_val);
 	assert(!ret);
 	assert(ret_val == PTHREAD_CANCELED);
+}
+
+int main() {
+	pthread_t thread;
+	int ret = pthread_create(&thread, NULL, &worker1, NULL);
+	assert(!ret);
+
+	while (!worker_ready);
+	ret = pthread_cancel(thread);
+	assert(!ret);
+	check_result(thread);
+
+	worker_ready = 0;
+	ret = pthread_create(&thread, NULL, &worker2, NULL);
+	assert(!ret);
+
+	while(!worker_ready);
+	ret = pthread_cancel(thread);
+	assert(!ret);
+	main_ready = 1;
+	check_result(thread);
+
+	worker_ready = 0;
+	ret = pthread_create(&thread, NULL, &worker3, NULL);
+	assert(!ret);
+
+	while(!worker_ready);
+	ret = pthread_cancel(thread);
+	assert(!ret);
+	main_ready = 1;
+	check_result(thread);
+
 	return 0;
 }
