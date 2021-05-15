b62a416721dc1209f8b0dc2eadadd6344580abd5
Matt Taylor <mstaveleytaylor@gmail.com>
2021-05-15 20:13:54 +0100

    options/posix: implement getpw{nam,uid}_r

--- options/posix/generic/pwd-stubs.cpp
+++ options/posix/generic/pwd-stubs.cpp
@@ -11,9 +11,11 @@ namespace {
 	thread_local passwd global_entry;
 
 	bool extract_entry(frg::string_view line, passwd *entry) {
-		__ensure(!entry->pw_name);
-		__ensure(!entry->pw_dir);
-		__ensure(!entry->pw_shell);
+		if (entry == &global_entry) {
+			__ensure(!entry->pw_name);
+			__ensure(!entry->pw_dir);
+			__ensure(!entry->pw_shell);
+		}
 
 		frg::string_view segments[8];
 
@@ -35,8 +37,8 @@ namespace {
 		if(n < 7)
 			return false;
 
-		// segments[1] is the password and segments[4] is the user description; both of those
-		// are not exported to struct passwd. The other segments are consumed below.
+		// segments[1] is the password which is not exported to struct passwd.
+		// The other segments are consumed below.
 
 		// TODO: Handle strndup() failure.
 		auto name = strndup(segments[0].data(), segments[0].size());
@@ -49,12 +51,16 @@ namespace {
 		if(!gid)
 			return false;
 
+		auto real_name = strndup(segments[4].data(), segments[4].size());
+		__ensure(real_name);
 		auto dir = strndup(segments[5].data(), segments[5].size());
 		__ensure(dir);
 		auto shell = strndup(segments[6].data(), segments[6].size());
 		__ensure(shell);
-		auto real_name = strndup(segments[7].data(), segments[7].size());
-		__ensure(real_name);
+
+		// Chop the newline off the end of shell
+		__ensure(strlen(shell) > 0);
+		shell[strlen(shell) - 1] = '\0';
 
 		entry->pw_name = name;
 		entry->pw_uid = *uid;
@@ -63,6 +69,21 @@ namespace {
 		entry->pw_shell = shell;
 		entry->pw_gecos = real_name;
 		return true;
+	}
+
+	void copy_to_buffer(passwd *pwd, char *buffer, size_t size) {
+		char *pw_dir = stpcpy(buffer, pwd->pw_name) + 1;
+		free(pwd->pw_name);
+		pwd->pw_name = buffer;
+
+		char *pw_shell = stpcpy(pw_dir, pwd->pw_dir) + 1;
+		free(pwd->pw_dir);
+		pwd->pw_dir = pw_dir;
+
+		char *end = stpcpy(pw_shell, pwd->pw_shell);
+		__ensure(end <= buffer + size);
+		free(pwd->pw_shell);
+		pwd->pw_shell = pw_shell;
 	}
 
 	void clear_entry(passwd *entry) {
@@ -101,9 +122,33 @@ struct passwd *getpwnam(const char *name) {
 	return nullptr;
 }
 
-int getpwnam_r(const char *, struct passwd *, char *, size_t, struct passwd **) {
-	__ensure(!"Not implemented");
-	__builtin_unreachable();
+int getpwnam_r(const char *name, struct passwd *pwd, char *buffer, size_t size, struct passwd **result) {
+	*result = nullptr;
+	auto file = fopen("/etc/passwd", "r");
+	if(!file) {
+		return EIO;
+	}
+
+	char line[512];
+	while(fgets(line, 512, file)) {
+		if(!extract_entry(line, pwd))
+			continue;
+		if(!strcmp(pwd->pw_name, name)) {
+			fclose(file);
+
+			size_t required_size = strlen(pwd->pw_name) + strlen(pwd->pw_dir)
+				+ strlen(pwd->pw_shell) + 3;
+			if (size < required_size)
+				return ERANGE;
+
+			copy_to_buffer(pwd, buffer, size);
+			*result = pwd;		
+			return 0;
+		}
+	}
+
+	fclose(file);
+	return 0;
 }
 
 struct passwd *getpwuid(uid_t uid) {
@@ -127,9 +172,33 @@ struct passwd *getpwuid(uid_t uid) {
 	return nullptr;
 }
 
-int getpwuid_r(uid_t, struct passwd *, char *, size_t, struct passwd **) {
-	__ensure(!"Not implemented");
-	__builtin_unreachable();
+int getpwuid_r(uid_t uid, struct passwd *pwd, char *buffer, size_t size, struct passwd **result) {
+	*result = nullptr;
+	auto file = fopen("/etc/passwd", "r");
+	if(!file) {
+		return EIO;
+	}
+
+	char line[512];
+	while(fgets(line, 512, file)) {
+		if(!extract_entry(line, pwd))
+			continue;
+		if(pwd->pw_uid == uid) {
+			fclose(file);
+
+			size_t required_size = strlen(pwd->pw_name) + strlen(pwd->pw_dir)
+				+ strlen(pwd->pw_shell) + 3;
+			if (size < required_size)
+				return ERANGE;
+
+			copy_to_buffer(pwd, buffer, size);
+			*result = pwd;		
+			return 0;
+		}
+	}
+
+	fclose(file);
+	return 0;
 }
 
 void setpwent(void) {
