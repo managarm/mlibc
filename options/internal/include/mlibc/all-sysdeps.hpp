131f39eb863f8b7e624b4acfafd86864ac81a1e2
Geert Custers <geert.aj.custers@gmail.com>
2021-07-21 07:35:39 +0000

    meson.build: make glibc option optional

--- options/internal/include/mlibc/all-sysdeps.hpp
+++ options/internal/include/mlibc/all-sysdeps.hpp
@@ -16,16 +16,14 @@
 #	include <mlibc/linux-sysdeps.hpp>
 #endif // __MLIBC_LINUX_OPTION
 
+#ifdef __MLIBC_GLIBC_OPTION
+#	include <mlibc/glibc-sysdeps.hpp>
+#endif // __MLIBC_GLIBC_OPTION
+
 #ifdef MLIBC_BUILDING_RTDL
 #	include <mlibc/rtdl-sysdeps.hpp>
 #endif // MLIBC_BUILDING_RTDL
 
-// TODO(geert): Make glibc optional
-// Fixes this hack which works around rtdl not
-// including the glibc option
-#ifndef MLIBC_BUILDING_RTDL
-#	include <mlibc/glibc-sysdeps.hpp>
-#endif // !MLIBC_BUILDING_RTDL
 #include <mlibc/internal-sysdeps.hpp>
 
 #endif // MLIBC_ALL_SYSDEPS
