6e6941b42ddf18742a2110c10e546eb2b6f0e6e2
Dennis Bonke <admin@dennisbonke.com>
2021-08-29 23:39:53 +0200

    options/posix: Implement shm_open and shm_unlink
    Signed-off-by: Dennis Bonke <admin@dennisbonke.com>

--- options/posix/generic/sys-mman-stubs.cpp
+++ options/posix/generic/sys-mman-stubs.cpp
@@ -1,5 +1,8 @@
 
 #include <errno.h>
+#include <limits.h>
+#include <pthread.h>
+#include <unistd.h>
 #include <sys/mman.h>
 #include <bits/ensure.h>
 
@@ -89,4 +92,41 @@ int munmap(void *pointer, size_t size) {
 	return 0;
 }
 
+// The implementation of shm_open and shm_unlink is taken from musl.
+namespace {
+	char *shm_mapname(const char *name, char *buf) {
+		char *p;
+		while(*name == '/')
+			name++;
+		if(*(p = strchrnul(name, '/')) || p == name ||
+			(p - name <= 2 && name[0] == '.' && p[-1] == '.')) {
+			errno = EINVAL;
+			return 0;
+		}
+		if(p - name > NAME_MAX) {
+			errno = ENAMETOOLONG;
+			return 0;
+		}
+		memcpy(buf, "/dev/shm/", 9);
+		memcpy(buf + 9, name, p - name + 1);
+		return buf;
+	}
+}
 
+int shm_open(const char *name, int flags, mode_t mode) {
+	int cs;
+	char buf[NAME_MAX + 10];
+	if(!(name = shm_mapname(name, buf)))
+		return -1;
+	pthread_setcancelstate(PTHREAD_CANCEL_DISABLE, &cs);
+	int fd = open(name, flags | O_NOFOLLOW | O_CLOEXEC | O_NONBLOCK, mode);
+	pthread_setcancelstate(cs, 0);
+	return fd;
+}
+
+int shm_unlink(const char *name) {
+	char buf[NAME_MAX + 10];
+	if(!(name = shm_mapname(name, buf)))
+		return -1;
+	return unlink(name);
+}
