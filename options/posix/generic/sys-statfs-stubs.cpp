0b8077aaf049bf678c5f901afc7423c4df9bbd64
Dennis Bonke <admin@dennisbonke.com>
2021-09-01 10:35:28 +0200

    options/posix: Stub statfs
    Signed-off-by: Dennis Bonke <admin@dennisbonke.com>

--- options/posix/generic/sys-statfs-stubs.cpp
+++ options/posix/generic/sys-statfs-stubs.cpp
@@ -1,10 +1,22 @@
 
+#include <errno.h>
 #include <sys/statfs.h>
 #include <bits/ensure.h>
 
-int statfs(const char *, struct statfs *) {
-	__ensure(!"Not implemented");
-	__builtin_unreachable();
+#include <mlibc/debug.hpp>
+#include <mlibc/posix-sysdeps.hpp>
+
+int statfs(const char *path, struct statfs *buf) {
+	if(!mlibc::sys_statfs) {
+		MLIBC_MISSING_SYSDEP();
+		errno = ENOSYS;
+		return -1;
+	}
+	if(int e = mlibc::sys_statfs(path, buf); e) {
+		errno = e;
+		return -1;
+	}
+	return 0;
 }
 
 int fstatfs(int, struct statfs *) {
