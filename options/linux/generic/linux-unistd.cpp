9467052650245cafa747df4ac776857ed7108f6e
ikbenlike <thewatcheriswatchinu@gmail.com>
2021-09-26 23:44:43 +0200

    linux: implement getdtablesize() using sysconf

--- options/linux/generic/linux-unistd.cpp
+++ options/linux/generic/linux-unistd.cpp
@@ -1,6 +1,8 @@
 #include <bits/linux/linux_unistd.h>
 #include <bits/ensure.h>
 
+#include <unistd.h>
+
 int dup3(int, int, int) {
 	__ensure(!"Not implemented");
 	__builtin_unreachable();
@@ -9,4 +11,8 @@ int dup3(int, int, int) {
 int vhangup(void) {
 	__ensure(!"Not implemented");
 	__builtin_unreachable();
+}
+
+int getdtablesize(void){
+	return sysconf(_SC_OPEN_MAX);
 }
