0b5331961c9e148678d5ef5b67843bf42bf16dcb
ikbenlike <thewatcheriswatchinu@gmail.com>
2021-10-07 01:56:48 +0200

    ansi: add wmemcmp() tests

---
+++ tests/ansi/wmemcmp.c
@@ -0,0 +1,17 @@
+#include <assert.h>
+#include <wchar.h>
+#include <stdlib.h>
+
+int main(void){
+    wchar_t *ws1 = calloc(10, sizeof(wchar_t));
+    wchar_t *ws2 = calloc(10, sizeof(wchar_t));
+
+    mbstowcs(ws1, "Test 1", 10);
+    mbstowcs(ws2, "Tester 2", 10);
+
+    assert(wmemcmp(ws1, ws2, 10) < 0);
+    assert(wmemcmp(ws2, ws1, 10) > 0);
+    assert(wmemcmp(ws2, ws2, 10) == 0);
+
+    return 0;
+}
