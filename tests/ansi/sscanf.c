ff7e69551856e933e883d4e1d516a23078ba3389
Geert Custers <geert.aj.custers@gmail.com>
2021-10-17 16:16:47 +0200

    options/ansi: fix scanf() hex reading

--- tests/ansi/sscanf.c
+++ tests/ansi/sscanf.c
@@ -3,29 +3,48 @@
 #include <assert.h>
 
 int main() {
-	int x = 0;
-	char buf1[] = "12345";
-	sscanf(buf1, "%d", &x);
-	assert(x == 12345);
+	{
+		int x = 0;
+		char buf[] = "12345";
+		sscanf(buf, "%d", &x);
+		assert(x == 12345);
+	}
 
-	char c;
-	int n1;
-	int n2;
-	char buf3[] = "z$ 7 5 440";;
-	int count = sscanf(buf3, "%*c%c %d %*d %d", &c, &n1, &n2);
-	assert(count == 3);
-	assert(c == '$');
-	assert(n1 == 7);
-	assert(n2 == 440);
+	{
+		char c;
+		int n1;
+		int n2;
+		char buf[] = "z$ 7 5 440";;
+		int count = sscanf(buf, "%*c%c %d %*d %d", &c, &n1, &n2);
+		assert(count == 3);
+		assert(c == '$');
+		assert(n1 == 7);
+		assert(n2 == 440);
+	}
 
-	// From dsda-doom
-	char buf2[] = "process_priority               0\n";
-	char def[80], strparm[128];
-	memset(def, '!', 80);
-	memset(strparm, '!', 128);
-	sscanf(buf2, "%s %[^\n]\n", def, strparm);
-	assert(!strcmp(def, "process_priority"));
-	assert(!strcmp(strparm, "0"));
+	{
+		// From dsda-doom
+		char buf[] = "process_priority               0\n";
+		char def[80], strparm[128];
+		memset(def, '!', 80);
+		memset(strparm, '!', 128);
+		sscanf(buf, "%s %[^\n]\n", def, strparm);
+		assert(!strcmp(def, "process_priority"));
+		assert(!strcmp(strparm, "0"));
+	}
 
+	{
+		char buf[] = "fffffffff100";
+		unsigned long y = 0;
+		sscanf(buf, "%lx", &y);
+		assert(y == 0xfffffffff100);
+	}
+
+	{
+		char buf[] = "410dc000";
+		unsigned long y = 0;
+		sscanf(buf, "%lx", &y);
+		assert(y == 0x410dc000);
+	}
 	return 0;
 }
