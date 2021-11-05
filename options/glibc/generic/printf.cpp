93428d57cbdb05ebf4aad563e45d081ba173dfb7
Dennis Bonke <admin@dennisbonke.com>
2021-11-05 15:37:25 +0100

    options/glibc: Add some printf and network related defines and structs
    Signed-off-by: Dennis Bonke <admin@dennisbonke.com>

---
+++ options/glibc/generic/printf.cpp
@@ -0,0 +1,7 @@
+#include <bits/ensure.h>
+#include <printf.h>
+
+size_t parse_printf_format(const char * __restrict, size_t, int * __restrict) {
+	__ensure(!"Not implemented");
+	__builtin_unreachable();
+}
