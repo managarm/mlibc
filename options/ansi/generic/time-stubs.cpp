0fc0d7444236c6b1240f4827a0a9fa1f74350e07
ikbenlike <thewatcheriswatchinu@gmail.com>
2021-11-13 04:23:31 +0100

    ansi: implement strptime()

--- options/ansi/generic/time-stubs.cpp
+++ options/ansi/generic/time-stubs.cpp
@@ -6,6 +6,7 @@
 #include <limits.h>
 #include <wchar.h>
 #include <stdlib.h>
+#include <ctype.h>
 
 #include <bits/ensure.h>
 #include <mlibc/debug.hpp>
@@ -564,13 +565,13 @@ char *asctime_r(const struct tm *tm, char *buf) {
 		{ "Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct",
 		  "Nov", "Dec" };
 	sprintf(buf, "%.3s %.3s%3d %.2d:%.2d%.2d %d\n",
-	             weekday_names[tm->tm_wday],
-	             month_names[tm->tm_mon],
-	             tm->tm_mday,
-	             tm->tm_hour,
-	             tm->tm_min,
-	             tm->tm_sec,
-	             tm->tm_year + 1900);
+				 weekday_names[tm->tm_wday],
+				 month_names[tm->tm_mon],
+				 tm->tm_mday,
+				 tm->tm_hour,
+				 tm->tm_min,
+				 tm->tm_sec,
+				 tm->tm_year + 1900);
 	return buf;
 }
 
@@ -578,10 +579,472 @@ char *ctime_r(const time_t *clock, char *buf) {
 	return asctime_r(localtime(clock), buf);
 }
 
-char *strptime(const char *__restrict, const char *__restrict,
-		struct tm *__restrict) {
-	mlibc::infoLogger() << "mlibc: strptime is a stub!" << frg::endlog;
-	return nullptr;
+namespace {
+
+int month_to_day(int month) {
+	switch(month){
+		case  0: return 0;
+		case  1: return 31;
+		case  2: return 59;
+		case  3: return 90;
+		case  4: return 120;
+		case  5: return 151;
+		case  6: return 181;
+		case  7: return 212;
+		case  8: return 243;
+		case  9: return 273;
+		case 10: return 304;
+		case 11: return 334;
+	}
+	return -1;
+}
+
+int is_leapyear(int year) {
+	return year % 4 == 0 && (year % 100 != 0 || year % 400 == 0);
+}
+
+int month_and_year_to_day_in_year(int month, int year){
+	int day = month_to_day(month);
+	if(is_leapyear(year) && month < 2)
+		return day + 1;
+
+	return day;
+}
+
+int target_determination(int month) {
+	switch(month){
+		case 0: return 3;
+		case 1: return 14;
+		case 2: return 14;
+		case 3: return 4;
+		case 4: return 9;
+		case 5: return 6;
+		case 6: return 11;
+		case 7: return 8;
+		case 8: return 5;
+		case 9: return 10;
+		case 10: return 7;
+		case 11: return 12;
+	}
+
+	return -1;
+}
+
+int doom_determination(int full_year) {
+	int century = full_year / 100;
+	int anchor = 2 + 5 * (century % 4) % 7;
+
+	int year = full_year % 100;
+
+	if(year % 2)
+		year += 11;
+
+	year /= 2;
+
+	if(year % 2)
+		year += 11;
+
+	return 7 - (year % 7) + anchor;
+}
+
+//Determine day of week through the doomsday algorithm.
+int day_determination(int day, int month, int year) {
+	int doom = doom_determination(year);
+	bool leap = is_leapyear(year);
+
+	int target = target_determination(month);
+	if(leap && month < 2)
+		target++;
+
+	int doom_dif = (day - target) % 7;
+	return (doom + doom_dif) % 7;
+}
+
+struct strptime_internal_state {
+	bool has_century;
+	bool has_year;
+	bool has_month;
+	bool has_day_of_month;
+	bool has_day_of_year;
+	bool has_day_of_week;
+
+	bool full_year_given;
+
+	int century;
+
+	size_t format_index;
+	size_t input_index;
+};
+
+char *strptime_internal(const char *__restrict input, const char *__restrict format,
+	struct tm *__restrict tm, struct strptime_internal_state *__restrict state) {
+	while(isspace(input[state->input_index]))
+		state->input_index++;
+	
+	if(input[state->input_index] == '\0')
+		return NULL;
+
+	while(format[state->format_index] != '\0'){
+		if(format[state->format_index] != '%'){
+			if(isspace(format[state->format_index])){
+				while(isspace(input[state->input_index++]));
+				state->input_index--;
+			}
+			else {
+				if(format[state->format_index] != input[state->input_index++])
+					return NULL;
+			}
+			state->format_index++;
+			continue;
+		}
+		state->format_index++;
+		switch(format[state->format_index]){
+			case '%':
+				if(input[state->input_index++] != '%')
+					return NULL;
+				break;
+			case 'a':
+			case 'A':
+				__ensure(!"strptime() %a and %A directives unimplemented.");
+				__builtin_unreachable();
+				break;
+			case 'b':
+			case 'B':
+			case 'h':
+				__ensure(!"strptime() %b, %B and %h directives unimplemented.");
+				__builtin_unreachable();
+				break;
+			case 'c':
+				__ensure(!"strptime() %c directive unimplemented.");
+				__builtin_unreachable();
+				break;
+			case 'C': {
+				int product = 0, n = 0;
+				sscanf(&input[state->input_index], "%d%n", &product, &n);
+				if(n == 0 || 2 < n)
+					return NULL;
+				state->input_index += n;
+				state->century = product;
+				state->has_century = true;
+				break;
+			}
+			case 'd': //`%d` and `%e` are equivalent
+			case 'e': {
+				int product = 0, n = 0;
+				sscanf(&input[state->input_index], "%d%n", &product, &n);
+				if(n == 0 || 2 < n)
+					return NULL;
+				state->input_index += n;
+				tm->tm_mday = product;
+				state->has_day_of_month = true;
+				break;
+			}
+			case 'D': { //equivalent to `%m/%d/%y`
+				size_t pre_fi = state->format_index;
+				state->format_index = 0;
+
+				char *result = strptime_internal(input, "%m/%d/%y", tm, state);
+				if(result == NULL)
+					return NULL;
+
+				state->format_index = pre_fi;
+				break;
+			}
+			case 'H': {
+				int product = 0, n = 0;
+				sscanf(&input[state->input_index], "%d%n", &product, &n);
+				if(n == 0 || 2 < n)
+					return NULL;
+				state->input_index += n;
+				tm->tm_hour = product;
+				break;
+			}
+			case 'I':
+				__ensure(!"strptime() %I directive unimplemented.");
+				__builtin_unreachable();
+				break;
+			case 'j': {
+				int product = 0, n = 0;
+				sscanf(&input[state->input_index], "%d%n", &product, &n);
+				if(n == 0 || 3 < n)
+					return NULL;
+				state->input_index += n;
+				tm->tm_yday = product - 1;
+				state->has_day_of_year = true;
+				break;
+			}
+			case 'm': {
+				int product = 0, n = 0;
+				sscanf(&input[state->input_index], "%d%n", &product, &n);
+				if(n == 0 || 2 < n)
+					return NULL;
+				state->input_index += n;
+				tm->tm_mon = product - 1;
+				state->has_month = true;
+				break;
+			}
+			case 'M': {
+				int product = 0, n = 0;
+				sscanf(&input[state->input_index], "%d%n", &product, &n);
+				if(n == 0 || 2 < n)
+					return NULL;
+				state->input_index += n;
+				tm->tm_min = product;
+				break;
+			}
+			case 'n':
+			case 't': {
+				size_t n = 0;
+				while(isspace(input[state->input_index++]))
+					n++;
+				if(n == 0)
+					return NULL;
+				state->input_index--;
+				break;
+			}
+			case 'p':
+				__ensure(!"strptime() %p directive unimplemented.");
+				__builtin_unreachable();
+				break;
+			case 'r':
+				__ensure(!"strptime() %r directive unimplemented.");
+				__builtin_unreachable();
+				break;
+			case 'R': { //equivalent to `%H:%M`
+				size_t pre_fi = state->format_index;
+				state->format_index = 0;
+
+				char *result = strptime_internal(input, "%H:%M", tm, state);
+				if(result == NULL)
+					return NULL;
+
+				state->format_index = pre_fi;
+				break;
+			}
+			case 'S': {
+				int product = 0, n = 0;
+				sscanf(&input[state->input_index], "%d%n", &product, &n);
+				if(n == 0 || 2 < n)
+					return NULL;
+				state->input_index += n;
+				tm->tm_sec = product;
+				break;
+			}
+			case 'T': { //equivalent to `%H:%M:%S`
+				size_t pre_fi = state->format_index;
+				state->format_index = 0;
+
+				char *result = strptime_internal(input, "%H:%M:%S", tm, state);
+				if(result == NULL)
+					return NULL;
+
+				state->format_index = pre_fi;
+				break;
+			}
+			case 'U':
+				__ensure(!"strptime() %U directive unimplemented.");
+				__builtin_unreachable();
+				break;
+			case 'w': {
+				int product = 0, n = 0;
+				sscanf(&input[state->input_index], "%d%n", &product, &n);
+				if(n == 0 || 1 < n)
+					return NULL;
+				state->input_index += n;
+				tm->tm_wday = product;
+				state->has_day_of_week = true;
+				break;
+			}
+			case 'W':
+				__ensure(!"strptime() %W directive unimplemented.");
+				__builtin_unreachable();
+				break;
+			case 'x':
+				__ensure(!"strptime() %x directive unimplemented.");
+				__builtin_unreachable();
+				break;
+			case 'X':
+				__ensure(!"strptime() %X directive unimplemented.");
+				__builtin_unreachable();
+				break;
+			case 'y': {
+				int product = 0, n = 0;
+				sscanf(&input[state->input_index], "%d%n", &product, &n);
+				if(n == 0 || 2 < n)
+					return NULL;
+				if(product < 69)
+					product += 100;
+				state->input_index += n;
+				tm->tm_year = product;
+				state->has_year = true;
+				break;
+			}
+			case 'Y': {                     
+				int product = 0, n = 0;
+				sscanf(&input[state->input_index], "%d%n", &product, &n);
+				if(n == 0 || 4 < n)
+					return NULL;
+				state->input_index += n;
+				tm->tm_year = product - 1900;
+				state->has_year = true;
+				state->has_century = true;
+				state->full_year_given = true;
+				state->century = product / 100;
+				break;
+			}
+			case 'F': { //GNU extensions
+				//equivalent to `%Y-%m-%d`
+				size_t pre_fi = state->format_index;
+				state->format_index = 0;
+
+				char *result = strptime_internal(input, "%Y-%m-%d", tm, state);
+				if(result == NULL)
+					return NULL;
+
+				state->format_index = pre_fi;
+				break;
+			}
+			case 'g':
+				__ensure(!"strptime() %g directive unimplemented.");
+				__builtin_unreachable();
+				break;
+			case 'G':
+				__ensure(!"strptime() %G directive unimplemented.");
+				__builtin_unreachable();
+				break;
+			case 'u': {
+				int product = 0, n = 0;
+				sscanf(&input[state->input_index], "%d%n", &product, &n);
+				if(n == 0 || 1 < n)
+					return NULL;
+				state->input_index += n;
+				tm->tm_wday = product - 1;
+				break;
+			}
+			case 'V':
+				__ensure(!"strptime() %V directive unimplemented.");
+				__builtin_unreachable();
+				break;
+			case 'z':
+				__ensure(!"strptime() %z directive unimplemented.");
+				__builtin_unreachable();
+				break;
+			case 'Z':
+				__ensure(!"strptime() %Z directive unimplemented.");
+				__builtin_unreachable();
+				break;
+			case 's': //end of GNU extensions
+				__ensure(!"strptime() %s directive unimplemented.");
+				__builtin_unreachable();
+				break;
+			case 'E': { //locale-dependent date & time representation
+				__ensure(!"strptime() %E* directives unimplemented.");
+				__builtin_unreachable();
+				state->format_index++;
+				switch(format[state->format_index]){
+					case 'c':
+						break;
+					case 'C':
+						break;
+					case 'x':
+						break;
+					case 'X':
+						break;
+					case 'y':
+						break;
+					case 'Y':
+						break;
+					default:
+						return NULL;
+				}
+			}
+			case 'O': { //locale-dependent numeric symbols
+				__ensure(!"strptime() %O* directives unimplemented.");
+				__builtin_unreachable();
+				state->format_index++;
+				switch(format[state->format_index]){
+					case 'd':
+					case 'e':
+						break;
+					case 'H':
+						break;
+					case 'I':
+						break;
+					case 'm':
+						break;
+					case 'M':
+						break;
+					case 'S':
+						break;
+					case 'U':
+						break;
+					case 'w':
+						break;
+					case 'W':
+						break;
+					case 'y':
+						break;
+					default:
+						return NULL;
+				}
+			}
+			default:
+				return NULL;
+		}
+		state->format_index++;
+	}
+
+	return (char*)input + state->input_index;
+}
+
+} //anonymous namespace
+
+char *strptime(const char *__restrict s, const char *__restrict format, struct tm *__restrict tm){
+	struct strptime_internal_state state = {};
+
+	char *result = strptime_internal(s, format, tm, &state);
+
+	if(result == NULL)
+		return NULL;
+
+	if(state.has_century && !state.full_year_given){
+		int full_year = state.century * 100;
+
+		if(state.has_year){
+			//Compensate for default century-adjustment of `%j` operand
+			if(tm->tm_year >= 100)
+				full_year += tm->tm_year - 100;
+			else
+				full_year += tm->tm_year;
+		}
+
+		tm->tm_year = full_year - 1900;
+
+		state.has_year = true;
+	}
+
+	if(state.has_month && !state.has_day_of_year){
+		int day = 0;
+		if(state.has_year)
+			day = month_and_year_to_day_in_year(tm->tm_mon, tm->tm_year);
+		else
+			day = month_to_day(tm->tm_mon);
+
+		tm->tm_yday = day + tm->tm_mday - 1;
+		state.has_day_of_year = true;
+	}
+
+	if(state.has_year && !state.has_day_of_week){
+		if(!state.has_month && !state.has_day_of_month){
+			tm->tm_wday = day_determination(0, 0, tm->tm_year + 1900);
+		}
+		else if(state.has_month && state.has_day_of_month){
+			tm->tm_wday = day_determination(tm->tm_mday, tm->tm_mon, tm->tm_year + 1900);
+		}
+		state.has_day_of_week = true;
+	}
+
+	return result;
 }
 
 time_t timelocal(struct tm *) {
