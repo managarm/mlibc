318844f77ef7a5c7ecb4dc7e292fdc4f94a46389
no92 <no92.mail@gmail.com>
2022-02-12 02:13:07 +0100

    options/ansi: add tests for wcs{ncasecmp,dup}

---
+++ tests/ansi/wcsncasecmp.c
@@ -0,0 +1,11 @@
+#include <assert.h>
+#include <wchar.h>
+
+int main() {
+	assert(!wcsncasecmp(L"foo", L"foo", 3));
+	assert(!wcsncasecmp(L"foo", L"FOO", 3));
+	assert(!wcsncasecmp(L"fooa", L"FOOB", 3));
+	assert(wcsncasecmp(L"foo", L"bar", 3));
+	assert(!wcsncasecmp(L"fooa", L"FOOA", 4));
+	assert(!wcsncasecmp(L"123abc", L"123AbC", 6));
+}
