798e50beb28a25bf3e294c4fb1af8feaef447125
Dennis Bonke <admin@dennisbonke.com>
2022-05-04 22:10:18 +0200

    options/linux: Add packet.h and add struct sockaddr_ll
    Signed-off-by: Dennis Bonke <admin@dennisbonke.com>

---
+++ options/linux/include/netpacket/packet.h
@@ -0,0 +1,14 @@
+#ifndef _NETPACKET_PACKET_H
+#define _NETPACKET_PACKET_H
+
+struct sockaddr_ll {
+	unsigned short int sll_family;
+	unsigned short int sll_protocol;
+	int sll_ifindex;
+	unsigned short int sll_hatype;
+	unsigned char sll_pkttype;
+	unsigned char sll_halen;
+	unsigned char sll_addr[8];
+};
+
+#endif // _NETPACKET_PACKET_H
