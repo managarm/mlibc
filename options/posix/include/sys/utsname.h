d76463f4999313d3e33299a5a0a3cdeae70700f9
streaksu <assemblyislaw@gmail.com>
2022-05-05 14:55:46 +0200

    options/posix: Implement uname

--- options/posix/include/sys/utsname.h
+++ options/posix/include/sys/utsname.h
@@ -2,17 +2,11 @@
 #ifndef _SYS_UTSNAME_H
 #define _SYS_UTSNAME_H
 
+#include <abi-bits/utsname.h>
+
 #ifdef __cplusplus
 extern "C" {
 #endif
-
-struct utsname {
-	char sysname[65];
-	char nodename[65];
-	char release[65];
-	char version[65];
-	char machine[65];
-};
 
 int uname(struct utsname *);
 
