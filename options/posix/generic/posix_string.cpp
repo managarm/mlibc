3edd9e79533d4e85c4662dfcf4197283ac472032
Dennis Bonke <admin@dennisbonke.com>
2022-05-07 01:37:37 +0200

    options/posix: Add strlcpy and strlcat
    Signed-off-by: Dennis Bonke <admin@dennisbonke.com>

--- options/posix/generic/posix_string.cpp
+++ options/posix/generic/posix_string.cpp
@@ -131,3 +131,24 @@ void *memrchr(const void *m, int c, size_t n) {
 	}
 	return 0;
 }
+
+// BSD extensions.
+// Taken from musl
+size_t strlcpy(char *d, const char *s, size_t n) {
+	char *d0 = d;
+
+	if(!n--)
+		goto finish;
+	for(; n && (*d=*s); n--, s++, d++);
+	*d = 0;
+finish:
+	return d-d0 + strlen(s);
+}
+
+size_t strlcat(char *d, const char *s, size_t n) {
+	size_t l = strnlen(d, n);
+	if(l == n) {
+		return l + strlen(s);
+	}
+	return l + strlcpy(d + l, s, n - l);
+}
