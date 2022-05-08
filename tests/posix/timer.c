601d4c926643e01e14cc3174df90943a92884970
Dennis Bonke <admin@dennisbonke.com>
2022-05-08 23:57:27 +0200

    options/posix: Implement timeradd, timersub, timerclear and timerisset
    Signed-off-by: Dennis Bonke <admin@dennisbonke.com>

---
+++ tests/posix/timer.c
@@ -0,0 +1,26 @@
+#define _GNU_SOURCE
+#include <stdio.h>
+#include <sys/time.h>
+#include <assert.h>
+
+int main() {
+	struct timeval a = {0, 0};
+	struct timeval b = {0, 0};
+	struct timeval res = {0, 0};
+	a.tv_sec = 10;
+	assert(timerisset(&a) == 1);
+	timerclear(&a);
+	assert(timerisset(&a) == 0);
+	a.tv_sec = 40;
+	a.tv_usec = 500;
+	b.tv_sec = 10;
+	b.tv_usec = 20;
+	timeradd(&a, &b, &res);
+	assert(res.tv_sec == 50);
+	assert(res.tv_usec == 520);
+	timerclear(&res);
+	timersub(&a, &b, &res);
+	assert(res.tv_sec == 30);
+	assert(res.tv_usec == 480);
+	return 0;
+}
