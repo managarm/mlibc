6d5043f41d9493b70ba3731676ca08216ebf4065
no92 <no92.mail@gmail.com>
2022-05-20 13:52:20 +0200

    tests: add test for setpriority

---
+++ tests/posix/setpriority.c
@@ -0,0 +1,27 @@
+#include <assert.h>
+#include <errno.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <sys/resource.h>
+#include <unistd.h>
+
+int main() {
+	errno = 0;
+	int original_priority = getpriority(PRIO_PROCESS, getpid());
+
+	assert(original_priority != -1 || !errno);
+
+	int ret = setpriority(PRIO_PROCESS, getpid(), original_priority + 1);
+
+	if(ret) {
+		fprintf(stderr, strerror(errno));
+		exit(1);
+	}
+
+	errno = 0;
+	int new_priority = getpriority(PRIO_PROCESS, getpid());
+
+	assert(new_priority != -1 || !errno);
+	assert(new_priority == original_priority + 1);
+}
