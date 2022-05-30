f237357795485d2f4ae8b07b60469b35df77e288
Matt Taylor <mstaveleytaylor@gmail.com>
2022-05-30 19:06:40 +0100

    riscv: implement sigaltstack

--- tests/posix/sigaltstack.c
+++ tests/posix/sigaltstack.c
@@ -38,7 +38,7 @@ int main() {
 #elif defined(__aarch64__)
 	asm volatile ("mov sp, %0\n\tstp x0, x1, [sp, #-16]!" :: "r"(uint64_t{0}));
 #elif defined(__riscv) && __riscv_xlen == 64
-	asm volatile ("unimp"); // TODO
+	asm volatile ("li sp, 0\n\tsd zero, 0(sp)");
 #else
 #	error Unknown architecture
 #endif
