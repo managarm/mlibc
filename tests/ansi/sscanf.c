5cac4a95a7e733d1fa24616fd52e741b78feac45
Kyota Poeti <kyota.exe@gmail.com>
2022-05-31 19:51:50 -0400

    tests/ansi: Add * ignored specifier to sscanf test

--- tests/ansi/sscanf.c
+++ tests/ansi/sscanf.c
@@ -8,6 +8,16 @@ int main() {
 	sscanf(buf1, "%d", &x);
 	assert(x == 12345);
 
+	char c;
+	int n1;
+	int n2;
+	char buf3[] = "z$ 7 5 440";;
+	int count = sscanf(buf3, "%*c%c %d %*d %d", &c, &n1, &n2);
+	assert(count == 3);
+	assert(c == '$');
+	assert(n1 == 7);
+	assert(n2 = 440);
+
 	// From dsda-doom
 	char buf2[] = "process_priority               0\n";
 	char def[80], strparm[128];
