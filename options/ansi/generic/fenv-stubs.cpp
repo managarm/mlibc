2ae2f4e9d3a8e33c6ff3cb00beb7b8c66a9e82d3
Dennis Bonke <admin@dennisbonke.com>
2022-06-06 01:24:48 +0200

    options/ansi: Implement several of the fenv family functions

--- options/ansi/generic/fenv-stubs.cpp
+++ options/ansi/generic/fenv-stubs.cpp
@@ -2,22 +2,11 @@
 #include <bits/ensure.h>
 #include <fenv.h>
 
-int feclearexcept(int) {
-	__ensure(!"Not implemented");
-	__builtin_unreachable();
-}
-
-int fegetenv(fenv_t *) {
-	__ensure(!"Not implemented");
-	__builtin_unreachable();
-}
+// The functions that are not in this file but are defined in the header
+// are implemented like musl does in assembly.
+extern "C" __attribute__((__visibility__("hidden"))) int __fesetround(int);
 
 int fegetexceptflag(fexcept_t *, int) {
-	__ensure(!"Not implemented");
-	__builtin_unreachable();
-}
-
-int fegetround(void) {
 	__ensure(!"Not implemented");
 	__builtin_unreachable();
 }
@@ -27,29 +16,25 @@ int feholdexcept(fenv_t *) {
 	__builtin_unreachable();
 }
 
-int feraiseexcept(int) {
-	__ensure(!"Not implemented");
-	__builtin_unreachable();
-}
-
-int fesetenv(const fenv_t *) {
-	__ensure(!"Not implemented");
-	__builtin_unreachable();
-}
-
 int fesetexceptflag(const fexcept_t *, int) {
 	__ensure(!"Not implemented");
 	__builtin_unreachable();
 }
 
-int fesetround(int) {
-	__ensure(!"Not implemented");
-	__builtin_unreachable();
-}
-
-int fetestexcept(int) {
-	__ensure(!"Not implemented");
-	__builtin_unreachable();
+int fesetround(int r) {
+	if (r != FE_TONEAREST
+#ifdef FE_DOWNWARD
+		&& r != FE_DOWNWARD
+#endif
+#ifdef FE_UPWARD
+		&& r != FE_UPWARD
+#endif
+#ifdef FE_TOWARDZERO
+		&& r != FE_TOWARDZERO
+#endif
+	)
+		return -1;
+	return __fesetround(r);
 }
 
 int feupdateenv(const fenv_t *) {
