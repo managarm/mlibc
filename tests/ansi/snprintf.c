0d8418a3269ab53f21cdfcc0068e7710e24e049f
Matt Taylor <mstaveleytaylor@gmail.com>
2022-06-20 17:23:41 +0100

    tests/snprintf: disable -Wformat-truncation

--- tests/ansi/snprintf.c
+++ tests/ansi/snprintf.c
@@ -8,9 +8,13 @@ int main() {
 	assert(strncmp("123456789", buffer, 10) == 0);
 	assert(ret == 9);
 
+	// We deliberately induce a warning here.
+#pragma GCC diagnostic push
+#pragma GCC diagnostic ignored "-Wformat-truncation"
 	ret = snprintf(buffer, 10, "%d", 1234567890);
 	assert(strncmp("123456789", buffer, 10) == 0);
 	assert(ret == 10);
+#pragma GCC diagnostic pop
 
 	// mlibc issue #118.
 	ret = snprintf(NULL, 0, "%d", 123456789);
