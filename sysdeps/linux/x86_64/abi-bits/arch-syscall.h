cceab0665e95744e622e2007ee9dac82c5475999
Dennis Bonke <admin@dennisbonke.com>
2022-06-22 03:39:02 +0200

    sysdeps/linux: Add SYS_futex

--- sysdeps/linux/x86_64/abi-bits/arch-syscall.h
+++ sysdeps/linux/x86_64/abi-bits/arch-syscall.h
@@ -92,6 +92,7 @@
 #define SYS_umount2 166
 #define SYS_reboot 169
 #define SYS_sethostname 170
+#define SYS_futex 202
 #define SYS_getdents64 217
 #define SYS_sys_futex 202
 #define SYS_fadvise64 221
