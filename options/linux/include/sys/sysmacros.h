578e11413c30967da4a68a4f93d3212807192e9a
Dennis Bonke <admin@dennisbonke.com>
2022-06-23 00:33:57 +0200

    options/internal: Include mlibc-config.h in inline-emitter.cpp options/linux: extern C the macros in sys/sysmacros.h

--- options/linux/include/sys/sysmacros.h
+++ options/linux/include/sys/sysmacros.h
@@ -1,6 +1,10 @@
 #ifndef _SYS_SYSMACROS_H
 #define _SYS_SYSMACROS_H
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 #include <bits/inline-definition.h>
 
 __MLIBC_INLINE_DEFINITION unsigned int __mlibc_dev_major(
@@ -23,5 +27,9 @@ __MLIBC_INLINE_DEFINITION unsigned long long int __mlibc_dev_makedev(
 #define major(dev) __mlibc_dev_major(dev)
 #define minor(dev) __mlibc_dev_minor(dev)
 #define makedev(major, minor) __mlibc_dev_makedev(major, minor)
+
+#ifdef __cplusplus
+}
+#endif
 
 #endif // _SYS_SYSMACROS_H
