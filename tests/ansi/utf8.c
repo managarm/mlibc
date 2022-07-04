b6445e151c6bbee8d154f42faf3ed55712a6894d
Matt Taylor <mstaveleytaylor@gmail.com>
2022-07-04 01:41:08 +0100

    tests/ansi: enhance utf8 test and re-enable for host libc

--- tests/ansi/utf8.c
+++ tests/ansi/utf8.c
@@ -3,28 +3,49 @@
 #include <stdlib.h>
 #include <string.h>
 #include <wchar.h>
+#include <locale.h>
 
-void verify_decode(const char *input, wchar_t expected) {
-	mbstate_t state;
+#define EXPECT(func) ({ \
+		if(res != n) { \
+			printf(#func " decoded %d bytes (expected %d bytes), at %s:%d\n", \
+					res, n, __FILE__, line); \
+			fflush(stdout); \
+			abort(); \
+		} \
+		if(wc != expected) { \
+			printf(#func " output cp %x (expected cp %x), at %s:%d\n", wc, \
+					expected, __FILE__, line); \
+			fflush(stdout); \
+			abort(); \
+		} \
+	})
+
+void verify_decode(const char *input, wchar_t expected, int line) {
 	wchar_t wc;
 	int n = strlen(input);
 	int res;
 
-	// Check that the conversion with mbrtowc() works.
-	memset(&state, 0, sizeof(mbstate_t));
+	// Check mbrtowc().
+	mbstate_t state;
+	memset(&state, 0, sizeof(state));
+	assert(mbrtowc(NULL, NULL, -1, &state) == 0);
 	res = mbrtowc(&wc, input, n, &state);
+	EXPECT("mbrtowc");
+	res = mbrtowc(NULL, input, n, &state);
+	EXPECT("mbrtowc (pwc == NULL)");
 
-	if(res != n) {
-		printf("mbtowc decoded %d bytes (expected %d bytes)\n", res, n);
-		abort();
-	}
-	if(wc != expected) {
-		printf("mbtowc returned cp %x (expected cp %x)\n", wc, expected);
-		abort();
-	}
+	// Check mbtowc().
+	assert(mbtowc(NULL, NULL, -1) == 0);
+	res = mbtowc(&wc, input, n);
+	EXPECT("mbtowc");
+	res = mbtowc(NULL, input, n);
+	EXPECT("mbtowc (pwc == NULL)");
 }
 
 int main() {
+	setlocale(LC_ALL, "C.UTF-8");
+
+#define verify_decode(in, out) verify_decode(in, out, __LINE__)
 	verify_decode("\x24", 0x24);
 	verify_decode("\xC2\xA2", 0xA2);
 	verify_decode("\xE0\xA4\xB9", 0x939);
