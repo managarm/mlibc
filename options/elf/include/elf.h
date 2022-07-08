4d03028f012be306e628aab29cfdfb7ea0c00b23
Matt Taylor <mstaveleytaylor@gmail.com>
2022-07-08 22:53:49 +0100

    options/elf: add Elf32_auxv_t

--- options/elf/include/elf.h
+++ options/elf/include/elf.h
@@ -3,6 +3,7 @@
 
 #include <stdint.h>
 #include <bits/inline-definition.h>
+#include <abi-bits/auxv.h>
 
 // TODO: Convert the enums to #defines so that they work with #ifdef.
 
@@ -412,6 +413,13 @@ typedef struct {
     uint64_t a_val;
   } a_un;
 } Elf64_auxv_t;
+
+typedef struct {
+	uint32_t a_type;
+	union {
+		uint32_t a_val;
+	} a_un;
+} Elf32_auxv_t;
 
 typedef struct {
   Elf32_Word n_namesz;
