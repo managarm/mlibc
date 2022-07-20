e6025045044cb4c60c1852050a4c796ee2cbf72f
Kacper Słomiński <kacper.slominski72@gmail.com>
2022-07-20 02:18:36 +0200

    options: fix TP offset on aarch64
    TP needs to be offset so that it points to 16 bytes before the end of
    TCB.

--- options/internal/aarch64-include/mlibc/thread.hpp
+++ options/internal/aarch64-include/mlibc/thread.hpp
@@ -6,9 +6,10 @@
 namespace mlibc {
 
 inline Tcb *get_current_tcb() {
+	// On AArch64, TPIDR_EL0 points to 0x10 bytes before the first TLS block.
 	uintptr_t ptr;
 	asm ("mrs %0, tpidr_el0" : "=r"(ptr));
-	return reinterpret_cast<Tcb *>(ptr);
+	return reinterpret_cast<Tcb *>(ptr + 0x10 - sizeof(Tcb));
 }
 
 } // namespace mlibc
