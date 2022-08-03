4f8d309118579ebb5d81f32dda102987799c3cf9
Matt Taylor <mstaveleytaylor@gmail.com>
2022-08-03 21:58:40 +0100

    internal: use libsmarter

--- options/internal/gcc-extra/cxxabi.cpp
+++ options/internal/gcc-extra/cxxabi.cpp
@@ -1,16 +1,18 @@
-
-#include <bits/ensure.h>
+#include <mlibc/allocator.hpp>
 
 // The cxxabi needs operator delete for *deleting* destructors, i.e., destructors that
 // are called by delete expressions. We never use such expressions in mlibc.
 // Note that G++ complains if we make the operator hidden,
-// thus we use it's mangled name as a workaround.
-#if defined(__clang__)
-	extern "C" [[gnu::visibility("hidden")]] void _ZdlPv() { // operator delete (void *, size_t)
-		__ensure(!"operator delete called! delete expressions cannot be used in mlibc.");
-	}
-#else
-	extern "C" [[gnu::visibility("hidden")]] void _ZdlPvm() { // operator delete (void *, size_t)
-		__ensure(!"operator delete called! delete expressions cannot be used in mlibc.");
-	}
-#endif
+// thus we use its mangled name as a workaround.
+
+extern "C" [[gnu::visibility("hidden")]] void *_Znwm(size_t size) { // operator new (size_t)
+	return getAllocator().allocate(size);
+}
+
+extern "C" [[gnu::visibility("hidden")]] void _ZdlPv(void *ptr) { // operator delete (void *)
+	getAllocator().free(ptr);
+}
+
+extern "C" [[gnu::visibility("hidden")]] void _ZdlPvm(void *ptr, size_t size) { // operator delete (void *, size_t)
+	getAllocator().deallocate(ptr, size);
+}
