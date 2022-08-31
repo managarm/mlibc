a5f052bb0a1c469a0dd5876b20042bdbffdd2eca
Moody Liu <mooodyhunter@outlook.com>
2022-08-31 18:56:52 +0100

    dummy-libs: fix typos in the comments (#731)

--- dummy-libs/libdl/src/dummy.cpp
+++ dummy-libs/libdl/src/dummy.cpp
@@ -1,5 +1,5 @@
 
-// We build an empty libdl because g++ always links with -lm
+// We build an empty libdl because g++ always links with -ldl
 // The actual functions reside inside libc
 
 extern "C" void __mlibc_libdl_dummy(void) { }
--- dummy-libs/libpthread/src/dummy.cpp
+++ dummy-libs/libpthread/src/dummy.cpp
@@ -1,5 +1,5 @@
 
-// We build an empty libpthread because g++ always links with -lm
+// We build an empty libpthread because g++ always links with -lpthread
 // The actual functions reside inside libc
 
 extern "C" void __mlibc_libpthread_dummy(void) { }
