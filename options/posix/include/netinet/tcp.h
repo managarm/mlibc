65b81250657debcba6e2b99e8be94625589bc104
Dennis Bonke <admin@dennisbonke.com>
2022-11-13 23:06:42 +0100

    options/posix: Define 3 additional tcp constants

--- options/posix/include/netinet/tcp.h
+++ options/posix/include/netinet/tcp.h
@@ -8,8 +8,11 @@ extern "C" {
 // Define some macros using same ABI as Linux
 #define TCP_NODELAY 1
 #define TCP_MAXSEG 2
+#define TCP_KEEPIDLE 4
 #define TCP_KEEPINTVL 5
 #define TCP_KEEPCNT 6
+#define TCP_DEFER_ACCEPT 9
+#define TCP_CONGESTION 13
 
 #define SOL_TCP 6
 
