2530fc7061e6f3b8f82164ff0d7db8fddaa66ae4
no92 <no92.mail@gmail.com>
2023-02-01 01:33:13 +0100

    options/ansi: move FE_* macros into a bit

--- options/ansi/include/fenv.h
+++ options/ansi/include/fenv.h
@@ -3,20 +3,7 @@
 #define _FENV_H
 
 #include <bits/types.h>
-
-#define FE_DIVBYZERO 4
-#define FE_INEXACT 32
-#define FE_INVALID 1
-#define FE_OVERFLOW 8
-#define FE_UNDERFLOW 16
-#define __FE_DENORM 2
-
-#define FE_ALL_EXCEPT 63
-
-#define FE_DOWNWARD 0x400
-#define FE_TONEAREST 0
-#define FE_TOWARDZERO 0xC00
-#define FE_UPWARD 0x800
+#include <bits/ansi/fenv.h>
 
 #ifdef __cplusplus
 extern "C" {
