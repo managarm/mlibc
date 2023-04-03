77958544f5c415f4a76a5a3d4ca21d09da9c351b
no92 <no92.mail@gmail.com>
2023-04-03 20:21:48 +0200

    options/linux: set up header for CPU_* macros
    Co-authored-by: Dennis Bonke <admin@dennisbonke.com>

---
+++ options/linux/include/bits/linux/cpu_set.h
@@ -0,0 +1,15 @@
+#ifndef _LINUX_CPU_SET_H
+#define _LINUX_CPU_SET_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#include <bits/cpu_set.h>
+#include <limits.h>
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* _LINUX_CPU_SET_H */
