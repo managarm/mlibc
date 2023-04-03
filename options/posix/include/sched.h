3a0ad3d09368513fdf864591d62cc5cb3a8dbed9
no92 <no92.mail@gmail.com>
2023-04-03 20:21:48 +0200

    options/linux: set up header for CPU_* macros
    Co-authored-by: Dennis Bonke <admin@dennisbonke.com>

--- options/posix/include/sched.h
+++ options/posix/include/sched.h
@@ -3,7 +3,6 @@
 #define _SCHED_H
 
 #include <abi-bits/pid_t.h>
-#include <bits/cpu_set.h>
 #include <bits/size_t.h>
 #include <mlibc-config.h>
 
@@ -11,12 +10,16 @@
 
 // MISSING: POSIX [PS], [SS] and [TSP] options
 
-#define CPU_SETSIZE 128
 #define CPU_ISSET __mlibc_cpu_isset
 #define CPU_COUNT __mlibc_cpu_count
 
 #ifdef __cplusplus
 extern "C" {
+#endif
+
+#if __MLIBC_LINUX_OPTION
+#include <bits/linux/linux_sched.h>
+#include <bits/linux/cpu_set.h>
 #endif
 
 #define SCHED_OTHER 0
@@ -42,10 +45,6 @@ int __mlibc_cpu_count(const cpu_set_t *set);
 int sched_setscheduler(pid_t pid, int policy, const struct sched_param *param);
 
 int sched_getparam(pid_t pid, struct sched_param *param);
-
-#if __MLIBC_LINUX_OPTION
-#include <bits/linux/linux_sched.h>
-#endif
 
 #ifdef __cplusplus
 }
