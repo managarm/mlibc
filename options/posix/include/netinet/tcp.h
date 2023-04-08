f531c5a4011d94d3d469c12abc3e12cbea359196
no92 <no92.mail@gmail.com>
2023-04-08 22:44:44 +0200

    options/posix: define TCP_* macros

--- options/posix/include/netinet/tcp.h
+++ options/posix/include/netinet/tcp.h
@@ -15,6 +15,18 @@ extern "C" {
 #define TCP_CONGESTION 13
 #define TCP_FASTOPEN 23
 
+#define TCP_ESTABLISHED 1
+#define TCP_SYN_SENT 2
+#define TCP_SYN_RECV 3
+#define TCP_FIN_WAIT1 4
+#define TCP_FIN_WAIT2 5
+#define TCP_TIME_WAIT 6
+#define TCP_CLOSE 7
+#define TCP_CLOSE_WAIT 8
+#define TCP_LAST_ACK 9
+#define TCP_LISTEN 10
+#define TCP_CLOSING 11
+
 #define SOL_TCP 6
 
 #ifdef __cplusplus
