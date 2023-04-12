99106baae450e53f6d5a9afdeff710b22e991383
no92 <no92.mail@gmail.com>
2023-04-12 12:16:49 +0200

    options/ansi: define basic types in thread.h

--- options/ansi/include/threads.h
+++ options/ansi/include/threads.h
@@ -5,6 +5,24 @@
 extern "C" {
 #endif
 
+enum {
+	mtx_plain,
+	mtx_recursive,
+	mtx_timed,
+};
+
+enum {
+	thrd_success,
+	thrd_timedout,
+	thrd_busy,
+	thrd_error,
+	thrd_nomem,
+};
+
+typedef struct __mlibc_thread_data * thrd_t;
+typedef struct __mlibc_mutex mtx_t;
+typedef struct __mlibc_cond cnd_t;
+
 #ifdef __cplusplus
 }
 #endif
