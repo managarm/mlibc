f98c766fb6ec36d7db069169d5897c8c105e0f5a
no92 <no92.mail@gmail.com>
2023-04-13 16:27:45 +0200

    options/ansi: add uchar.h

---
+++ options/ansi/include/uchar.h
@@ -0,0 +1,19 @@
+#ifndef _UCHAR_H
+#define _UCHAR_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#include <bits/size_t.h>
+
+#ifndef __cplusplus
+typedef __CHAR16_TYPE__ char16_t;
+typedef __CHAR32_TYPE__ char32_t;
+#endif /* __cplusplus */
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* _UCHAR_H */
