eedffe680819bbad57bda129965a5df964b567a9
Alexander Richards <Alexander.Richards@itdesign.at>
2023-07-13 14:34:27 +0200

    managarm: fix a stub in setsockopt

--- sysdeps/managarm/generic/socket.cpp
+++ sysdeps/managarm/generic/socket.cpp
@@ -352,8 +352,8 @@ int sys_setsockopt(int fd, int layer, int number,
 	}else if(layer == AF_NETLINK && number == SO_ACCEPTCONN) {
 		mlibc::infoLogger() << "\e[31mmlibc: setsockopt() call with AF_NETLINK and SO_ACCEPTCONN is unimplemented\e[39m" << frg::endlog;
 		return 0;
-	}else if(layer == AF_NETLINK && number == SO_ERROR) {
-		mlibc::infoLogger() << "\e[31mmlibc: setsockopt() call with AF_NETLINK and SO_ERROR is unimplemented\e[39m" << frg::endlog;
+	}else if(layer == IPPROTO_TCP && number == TCP_NODELAY) {
+		mlibc::infoLogger() << "\e[31mmlibc: setsockopt() call with IPPROTO_TCP and TCP_NODELAY is unimplemented\e[39m" << frg::endlog;
 		return 0;
 	}else if(layer == SOL_SOCKET && number == SO_ACCEPTCONN) {
 		mlibc::infoLogger() << "\e[31mmlibc: setsockopt() call with SOL_SOCKET and SO_ACCEPTCONN is unimplemented\e[39m" << frg::endlog;
