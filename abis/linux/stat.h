4742c72e2f217c33f830446175682d4763796bcf
Fedor Lapshin <fe.lap.prog@gmail.com>
2023-08-15 18:09:33 +0300

    abis/linux: define `struct stat` for x86

--- abis/linux/stat.h
+++ abis/linux/stat.h
@@ -85,6 +85,31 @@ struct stat {
 	unsigned int __unused5;
 };
 
+#elif defined(__i386__)
+
+struct stat {
+	dev_t st_dev;
+	int __st_dev_padding;
+	long __st_ino_truncated;
+	mode_t st_mode;
+	nlink_t st_nlink;
+	uid_t st_uid;
+	gid_t st_gid;
+	dev_t st_rdev;
+	int __st_rdev_padding;
+	off_t st_size;
+	blksize_t st_blksize;
+	blkcnt_t st_blocks;
+	struct {
+		long tv_sec;
+		long tv_nsec;
+	} __st_atim32, __st_mtim32, __st_ctim32;
+	ino_t st_ino;
+	struct timespec st_atim;
+	struct timespec st_mtim;
+	struct timespec st_ctim;
+};
+
 #endif
 
 #ifdef __cplusplus
