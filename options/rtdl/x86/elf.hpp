5bb41b4681d3bd43b8885551a100d62ab4a618f8
no92 <no92.mail@gmail.com>
2023-08-29 01:37:54 +0200

    rtdl: abstract away some ELF types for 32-bit support
    With the `options/rtdl/<arch>/elf.hpp` file, the types necessary for
    parsing ELF on that arch are defined there. This allows for supporting
    32-bit with rtdl now. In the future, defining more information about the
    architecture there might prove useful, e.g. for checking ELF ehdr fields
    like class and machine.

---
+++ options/rtdl/x86/elf.hpp
@@ -0,0 +1,35 @@
+#pragma once
+
+#include <elf.h>
+
+#define ELF_CLASS 32
+
+using elf_ehdr = Elf32_Ehdr;
+using elf_phdr = Elf32_Phdr;
+using elf_dyn = Elf32_Dyn;
+using elf_rel = Elf32_Rel;
+using elf_rela = Elf32_Rela;
+using elf_sym = Elf32_Sym;
+using elf_addr = Elf32_Addr;
+
+using elf_info = Elf32_Word;
+using elf_addend = Elf32_Sword;
+
+#define ELF_R_SYM ELF32_R_SYM
+#define ELF_R_TYPE ELF32_R_TYPE
+#define ELF_ST_BIND ELF32_ST_BIND
+
+#define R_NONE R_386_NONE
+#define R_JUMP_SLOT R_386_JMP_SLOT
+#define R_ABSOLUTE R_386_32
+#define R_GLOB_DAT R_386_GLOB_DAT
+#define R_RELATIVE R_386_RELATIVE
+#define R_IRELATIVE R_386_IRELATIVE
+#define R_OFFSET R_386_PC32
+#define R_COPY R_386_COPY
+#define R_TLS_DTPMOD R_386_TLS_DTPMOD32
+#define R_TLS_DTPREL R_386_TLS_DTPOFF32
+#define R_TLS_TPREL R_386_TLS_TPOFF
+#define R_TLSDESC R_386_TLS_DESC
+
+#define TP_TCB_OFFSET 0
