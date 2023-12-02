e7f1bfa9229d40a8bb78dcf532f9e5dd1e7ee9e6
mintsuki <mintsuki@protonmail.com>
2023-12-02 01:32:33 +0100

    options/ansi: Move complex-stubs.cpp to complex-stubs.c
    This fixes an issue where the 'complex' type is not defined for C++ sources

--- options/ansi/generic/complex-stubs.c
+++ options/ansi/generic/complex-stubs.c
@@ -1,14 +1,9 @@
 #include <complex.h>
 
-extern "C" {
-
 long double cimagl(long double complex z) {
     return __imag__(z);
 }
 
 long double creall(long double complex z) {
     return __real__(z);
-}
-
-
 }
