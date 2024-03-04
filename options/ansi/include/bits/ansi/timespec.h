6774dcf6738396867a290f1538bee082c77bc8d9
Fedor Lapshin <fe.lap.prog@gmail.com>
2024-03-04 23:55:11 +0300

    options/ansi: add a comment about layout of timespec

--- options/ansi/include/bits/ansi/timespec.h
+++ options/ansi/include/bits/ansi/timespec.h
@@ -5,8 +5,13 @@
 #include <bits/ansi/time_t.h>
 #include <bits/field-padding.h>
 
+// Equivalent of timespec64 in glibc.
+// Should be used only with 64-bit syscalls
+// or with appropriate compat syscalls.
 struct timespec {
 	time_t tv_sec;
+	// tv_nspec is required to be long by the C standard.
+	// However linux kernel expects long long. So we add padding.
 	__MLIBC_FIELD_PADDED(long, tv_nsec, long long);
 };
 
