082ab672e6664e1cb7da26e74f91f397dd1f988e
Dennis Bonke <admin@dennisbonke.com>
2024-06-10 01:28:25 +0200

    options/internal: Add support for D_T_FMT in nl_langinfo

--- options/internal/generic/locale.cpp
+++ options/internal/generic/locale.cpp
@@ -79,6 +79,8 @@ char *nl_langinfo(nl_item item) {
 		return const_cast<char *>("%H:%M:%S");
 	}else if(item == T_FMT_AMPM) {
 		return const_cast<char *>("%I:%M:%S %p");
+	}else if(item == D_T_FMT) {
+		return const_cast<char *>("%a %b %e %T %Y");
 	}else{
 		mlibc::infoLogger() << "mlibc: nl_langinfo item "
 				<< item << " is not implemented properly" << frg::endlog;
