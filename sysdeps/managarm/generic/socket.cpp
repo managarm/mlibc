653e55e9920f3d913b057497bee605837dbc6d72
Dennis Bonke <admin@dennisbonke.com>
2024-06-10 03:01:20 +0200

    sysdeps/managarm: Stub some more setsockopt calls

--- sysdeps/managarm/generic/socket.cpp
+++ sysdeps/managarm/generic/socket.cpp
@@ -484,6 +484,9 @@ int sys_setsockopt(int fd, int layer, int number,
 	}else if(layer == SOL_SOCKET && number == SO_SNDBUF) {
 		mlibc::infoLogger() << "\e[31mmlibc: setsockopt() call with SOL_SOCKET and SO_SNDBUF is unimplemented\e[39m" << frg::endlog;
 		return 0;
+	}else if(layer == SOL_SOCKET && number == SO_SNDBUFFORCE) {
+		mlibc::infoLogger() << "\e[31mmlibc: setsockopt() call with SOL_SOCKET and SO_SNDBUFFORCE is unimplemented\e[39m" << frg::endlog;
+		return 0;
 	}else if(layer == SOL_SOCKET && number == SO_KEEPALIVE) {
 		mlibc::infoLogger() << "\e[31mmlibc: setsockopt() call with SOL_SOCKET and SO_KEEPALIVE is unimplemented\e[39m" << frg::endlog;
 		return 0;
@@ -516,6 +519,9 @@ int sys_setsockopt(int fd, int layer, int number,
 		return 0;
 	}else if(layer == IPPROTO_TCP && number == TCP_KEEPCNT) {
 		mlibc::infoLogger() << "\e[31mmlibc: setsockopt() call with IPPROTO_TCP and TCP_KEEPCNT is unimplemented\e[39m" << frg::endlog;
+		return 0;
+	}else if(layer == SOL_NETLINK && number == NETLINK_ADD_MEMBERSHIP) {
+		mlibc::infoLogger() << "\e[31mmlibc: setsockopt() call with SOL_NETLINK and NETLINK_ADD_MEMBERSHIP is unimplemented\e[39m" << frg::endlog;
 		return 0;
 	}else{
 		mlibc::panicLogger() << "\e[31mmlibc: Unexpected setsockopt() call, layer: " << layer << " number: " << number << "\e[39m" << frg::endlog;
