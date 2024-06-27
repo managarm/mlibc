ec0428d70186e2fe448392b02b759e1cee149e4d
Dennis Bonke <admin@dennisbonke.com>
2024-06-27 10:14:12 +0200

    options/linux: Stub signalfd where fd != -1
    Co-authored-by: no92 <leo@managarm.org>

--- options/linux/generic/sys-signalfd.cpp
+++ options/linux/generic/sys-signalfd.cpp
@@ -3,9 +3,14 @@
 #include <sys/signalfd.h>
 
 #include <bits/ensure.h>
+#include <mlibc/debug.hpp>
 #include <mlibc/linux-sysdeps.hpp>
 
 int signalfd(int fd, const sigset_t *mask, int flags) {
+	if(fd != -1) {
+		mlibc::infoLogger() << "mlibc: signalfd with fd != -1 is not supported yet" << frg::endlog;
+		return 0;
+	}
 	__ensure(fd == -1);
 	MLIBC_CHECK_OR_ENOSYS(mlibc::sys_signalfd_create, -1);
 	if(int e = mlibc::sys_signalfd_create(mask, flags, &fd); e) {
