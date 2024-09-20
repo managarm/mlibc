43821bfdb41eb96b40ef10fdb15b3335bdf9db1e
Dennis Bonke <admin@dennisbonke.com>
2024-09-20 14:22:06 +0200

    options/glibc: Define struct timeb

--- options/glibc/include/sys/timeb.h
+++ options/glibc/include/sys/timeb.h
@@ -1,11 +1,18 @@
 #ifndef _SYS_TIMEB_H
 #define _SYS_TIMEB_H
 
+#include <bits/ansi/time_t.h>
+
 #ifdef __cplusplus
 extern "C" {
 #endif
 
-
+struct timeb {
+	time_t time;
+	unsigned short int millitm;
+	short int timezone;
+	short int dstflag;
+};
 
 #ifdef __cplusplus
 }
