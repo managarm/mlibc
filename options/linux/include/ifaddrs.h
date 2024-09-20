8d20c10efe17e9a34cd05125a516bafbede3d8de
Dennis Bonke <admin@dennisbonke.com>
2024-09-20 15:19:07 +0200

    options/linux: Match struct ifaddrs against glibc

--- options/linux/include/ifaddrs.h
+++ options/linux/include/ifaddrs.h
@@ -9,17 +9,19 @@ extern "C" {
 #include <netinet/in.h>
 #include <sys/socket.h>
 
-// Struct definitions taken from musl
+// Struct definition taken from glibc
 struct ifaddrs {
 	struct ifaddrs *ifa_next;
 	char *ifa_name;
 	unsigned ifa_flags;
 	struct sockaddr *ifa_addr;
 	struct sockaddr *ifa_netmask;
-	// the man page (and glibc) place `ifa_broadaddr` and `ifa_dstaddr` in a union
-	// TODO: decide whether we should do it, too
-	struct sockaddr *ifa_broadaddr;
-	struct sockaddr *ifa_dstaddr;
+	union {
+		struct sockaddr *ifu_broadaddr;
+		struct sockaddr *ifu_dstaddr;
+	} ifa_ifu;
+#define ifa_broadaddr ifa_ifu.ifu_broadaddr
+#define ifa_dstaddr ifa_ifu.ifu_dstaddr
 	void *ifa_data;
 };
 
