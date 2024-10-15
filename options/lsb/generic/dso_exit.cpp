348e8a2d70e63dc4991bb5276beacfe4f9467e25
Matt Staveley-Taylor <matt.stav.taylor@gmail.com>
2024-10-15 22:06:34 +0200

    rtld: call all atexit handlers on process exit

--- options/lsb/generic/dso_exit.cpp
+++ options/lsb/generic/dso_exit.cpp
@@ -36,36 +36,45 @@ extern "C" void __dlapi_exit();
 
 extern "C" void __cxa_finalize(void *dso) {
 	ExitQueue &eq = getExitQueue();
-	if(!dso) {
-		for(size_t i = eq.size(); i > 0; i--) {
-			auto handler = &eq[i - 1];
-			if(!handler->function)
-				continue;
+	for (size_t i = eq.size(); i > 0; i--) {
+		auto &handler = eq[i - 1];
+		if (!handler.function)
+			continue;
 
-			handler->function(handler->argument);
-			handler->function = nullptr;
-		}
-	}else {
-		for(size_t i = eq.size(); i > 0; i--) {
-			auto handler = &eq[i - 1];
-			if(handler->dsoHandle != dso || !handler->function)
-				continue;
-
-			handler->function(handler->argument);
-			handler->function = nullptr;
+		if (!dso || handler.dsoHandle == dso) {
+			handler.function(handler.argument);
+			handler.function = nullptr;
 		}
 	}
 }
 
+extern "C" void *__dso_handle;
+
+[[gnu::destructor]] void __mlibc_do_destructors() {
+	// In normal programs this call to __cxa_finalize is provided by libgcc.
+	__cxa_finalize(&__dso_handle);
+}
+
 void __mlibc_do_finalize() {
+	// Invoke any handlers registered with atexit (NOT associated with a DSO).
+	// Note that we deliberately do not invoke other handlers here, since
+	// that would destroy mlibc's global objects including stdout and flushing
+	// open FILEs, but we'd like those to be available to [[gnu::destructor]]
+	// functions which we invoke below.
 	ExitQueue &eq = getExitQueue();
-	for(size_t i = eq.size(); i > 0; i--) {
-		auto handler = &eq[i - 1];
-		if(handler->dsoHandle || !handler->function)
+	for (size_t i = eq.size(); i > 0; i--) {
+		auto &handler = eq[i - 1];
+		if (!handler.function)
 			continue;
-		handler->function(handler->argument);
-		handler->function = nullptr;
+
+		if (!handler.dsoHandle) {
+			handler.function(handler.argument);
+			handler.function = nullptr;
+		}
 	}
 
+	// Call fini/fini_array functions of each loaded object. This is necessary
+	// to implement [[gnu::destructor]]. Note that C++ applications will call
+	// __cxa_finalize from here.
 	__dlapi_exit();
 }
