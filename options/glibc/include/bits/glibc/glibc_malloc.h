7474804805b41d92e28ac582a33eedf2e13b40dc
no92 <leo@managarm.org>
2024-11-22 16:56:46 +0100

    options/glibc: stub malloc_info

--- options/glibc/include/bits/glibc/glibc_malloc.h
+++ options/glibc/include/bits/glibc/glibc_malloc.h
@@ -6,8 +6,10 @@ extern "C" {
 #endif
 
 #include <bits/size_t.h>
+#include <bits/file.h>
 
 size_t malloc_usable_size(void *__ptr);
+int malloc_info(int options, FILE *stream);
 
 #ifdef __cplusplus
 }
