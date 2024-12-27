552aedaa654c0c2114f6d76ca136df672164fbb8
Kacper Słomiński <kacper.slominski72@gmail.com>
2024-12-27 19:48:01 +0100

    internal: Add missing volatile to inline assembly
    Without this, the compiler can (and will) hoist the inline assembly in
    potentially broken ways.

--- options/internal/aarch64-include/mlibc/thread.hpp
+++ options/internal/aarch64-include/mlibc/thread.hpp
@@ -8,13 +8,13 @@ namespace mlibc {
 inline Tcb *get_current_tcb() {
 	// On AArch64, TPIDR_EL0 points to 0x10 bytes before the first TLS block.
 	uintptr_t ptr;
-	asm ("mrs %0, tpidr_el0" : "=r"(ptr));
+	asm volatile ("mrs %0, tpidr_el0" : "=r"(ptr));
 	return reinterpret_cast<Tcb *>(ptr + 0x10 - sizeof(Tcb));
 }
 
 inline uintptr_t get_sp() {
 	uintptr_t sp;
-	asm ("mov %0, sp" : "=r"(sp));
+	asm volatile ("mov %0, sp" : "=r"(sp));
 	return sp;
 }
 
