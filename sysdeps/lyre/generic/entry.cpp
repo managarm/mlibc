00097cccbc18769f4b76212f28360cfe871bf708
Matt Staveley-Taylor <matt.stav.taylor@gmail.com>
2024-12-28 22:28:47 +0100

    sysdeps: invoke entry stack parsing from gnu::constructor

--- sysdeps/lyre/generic/entry.cpp
+++ sysdeps/lyre/generic/entry.cpp
@@ -7,24 +7,9 @@
 #include <bits/posix/posix_signal.h>
 #include <lyre/syscall.h>
 
-extern "C" uintptr_t *__dlapi_entrystack();
 extern "C" void __dlapi_enter(uintptr_t *);
 
 extern char **environ;
-static mlibc::exec_stack_data __mlibc_stack_data;
-
-struct LibraryGuard {
-	LibraryGuard();
-};
-
-static LibraryGuard guard;
-
-LibraryGuard::LibraryGuard() {
-	// Parse the exec() stack.
-	mlibc::parse_exec_stack(__dlapi_entrystack(), &__mlibc_stack_data);
-	mlibc::set_startup_data(__mlibc_stack_data.argc, __mlibc_stack_data.argv,
-			__mlibc_stack_data.envp);
-}
 
 struct GPRState {
 	uint64_t ds;
@@ -111,7 +96,7 @@ extern "C" void __mlibc_entry(uintptr_t *entry_stack, int (*main_fn)(int argc, c
 
 	//mlibc::sys_sigentry((void *)__mlibc_sigentry);
 
-	auto result = main_fn(__mlibc_stack_data.argc, __mlibc_stack_data.argv, environ);
+	auto result = main_fn(mlibc::entry_stack.argc, mlibc::entry_stack.argv, environ);
 	exit(result);
 }
 
--- sysdeps/managarm/generic/entry.cpp
+++ sysdeps/managarm/generic/entry.cpp
@@ -104,26 +104,11 @@ HelHandle getHandleForFd(int fd) {
 
 void clearCachedInfos() { has_cached_infos = PTHREAD_ONCE_INIT; }
 
-struct LibraryGuard {
-	LibraryGuard();
-};
-
-static LibraryGuard guard;
-
 extern char **environ;
-static mlibc::exec_stack_data __mlibc_stack_data;
-
-LibraryGuard::LibraryGuard() {
-	// Parse the exec() stack.
-	mlibc::parse_exec_stack(__dlapi_entrystack(), &__mlibc_stack_data);
-	mlibc::set_startup_data(
-	    __mlibc_stack_data.argc, __mlibc_stack_data.argv, __mlibc_stack_data.envp
-	);
-}
 
 extern "C" void
 __mlibc_entry(uintptr_t *entry_stack, int (*main_fn)(int argc, char *argv[], char *env[])) {
 	__dlapi_enter(entry_stack);
-	auto result = main_fn(__mlibc_stack_data.argc, __mlibc_stack_data.argv, environ);
+	auto result = main_fn(mlibc::entry_stack.argc, mlibc::entry_stack.argv, environ);
 	exit(result);
 }
