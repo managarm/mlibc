91f84fc379502c0c94a64f11f9612d13714ecb54
Omar Berrow <omarkberrow@gmail.com>
2025-01-12 23:44:06 -0500

    fix bug

--- sysdeps/obos/generic/sysdeps.cpp
+++ sysdeps/obos/generic/sysdeps.cpp
@@ -391,9 +391,11 @@ int sys_open(const char *pathname, int flags, mode_t mode, int *fd)
     (void)(mode);
     uint32_t real_flags = 0;
 
-    if (flags == O_RDONLY)
+    if ((flags & 3) == O_RDONLY)
         real_flags |= 1 /*FD_OFLAGS_READ*/;
-    if (flags & O_RDWR)
+    if ((flags & 3) == O_WRONLY)
+        real_flags |= 2 /*FD_OFLAGS_WRITE*/;
+    if ((flags & 3) == O_RDWR)
         real_flags |= 1|2 /*FD_OFLAGS_READ|FD_OFLAGS_WRITE*/;
     if (flags & O_CLOEXEC)
         real_flags |= 8 /* FD_OFLAGS_NOEXEC */;
