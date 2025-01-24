de67501ac503becc642588f025dd4066e12e0c3e
Dennis Bonke <admin@dennisbonke.com>
2025-01-24 01:19:23 +0100

    sysdeps/managarm: Improve error handling in sys_msg_recv and sys_statx

--- sysdeps/managarm/generic/file.cpp
+++ sysdeps/managarm/generic/file.cpp
@@ -927,8 +927,8 @@ int sys_msg_recv(int sockfd, struct msghdr *hdr, int flags, ssize_t *length) {
 	managarm::fs::RecvMsgReply<MemoryAllocator> resp(getSysdepsAllocator());
 	resp.ParseFromArray(recv_resp.data(), recv_resp.length());
 
-	if (resp.error() == managarm::fs::Errors::WOULD_BLOCK) {
-		return EAGAIN;
+	if (resp.error() != managarm::fs::Errors::SUCCESS) {
+		return resp.error() | toErrno;
 	} else {
 		__ensure(resp.error() == managarm::fs::Errors::SUCCESS);
 		HEL_CHECK(recv_addr.error());
@@ -2009,13 +2009,9 @@ sys_statx(int dirfd, const char *pathname, int flags, unsigned int mask, struct 
 
 	managarm::posix::SvrResponse<MemoryAllocator> resp(getSysdepsAllocator());
 	resp.ParseFromArray(recv_resp.data(), recv_resp.length());
-	if (resp.error() == managarm::posix::Errors::FILE_NOT_FOUND) {
-		return ENOENT;
-	} else if (resp.error() == managarm::posix::Errors::BAD_FD) {
-		return EBADF;
-	} else if (resp.error() == managarm::posix::Errors::NOT_A_DIRECTORY) {
-		return ENOTDIR;
-	} else {
+	if (resp.error() != managarm::posix::Errors::SUCCESS)
+		return resp.error() | toErrno;
+	else {
 		__ensure(resp.error() == managarm::posix::Errors::SUCCESS);
 		memset(statxbuf, 0, sizeof(struct statx));
 
