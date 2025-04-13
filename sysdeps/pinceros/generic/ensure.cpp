47f2250f39b38921f2c5aa7e3d7f88c13dcd74b4
Caleb Eden <58373773+calebeden@users.noreply.github.com>
2025-04-12 23:27:31 -0500

    mlibc debugging functions

--- sysdeps/pinceros/generic/ensure.cpp
+++ sysdeps/pinceros/generic/ensure.cpp
@@ -7,16 +7,36 @@
 #include <mlibc/debug.hpp>
 #include <mlibc/all-sysdeps.hpp>
 
+#include <aarch64/syscall.h>
+
 namespace mlibc
 {
-    void sys_libc_log(const char * /* message */)
+    void sys_libc_log(const char *message)
     {
-        MLIBC_UNIMPLEMENTED();
+        ssize_t n = 0;
+        while (message[n])
+            n++;
+
+        ssize_t total_written = 0;
+        while (total_written < n)
+        {
+            ssize_t bytes_written = 0;
+            if (sys_pwrite(2, message + total_written, n - total_written, 0, &bytes_written))
+            {
+                sys_libc_panic();
+            }
+            total_written += bytes_written;
+        }
+        char lf = '\n';
+        ssize_t bytes_written = 0;
+        if (sys_pwrite(2, &lf, 1, 0, &bytes_written))
+        {
+            sys_libc_panic();
+        }
     }
 
     [[noreturn]] void sys_libc_panic()
     {
-        MLIBC_UNIMPLEMENTED();
-        __builtin_unreachable();
+        __builtin_trap();
     }
 }
