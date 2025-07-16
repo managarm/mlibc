f83b8209bc3ff740aaadf16120a2b9f7cb93a2eb
no92 <leo@managarm.org>
2025-07-17 01:10:27 +0200

    options/{posix,linux}: implement utmp and utmpx

---
+++ abis/vinix/utmp-defines.h
@@ -0,0 +1,14 @@
+#ifndef _ABIBITS_UTMP_DEFINES_H
+#define _ABIBITS_UTMP_DEFINES_H
+
+#define EMPTY 0
+#define RUN_LVL 1
+#define BOOT_TIME 2
+#define NEW_TIME 3
+#define OLD_TIME 4
+#define INIT_PROCESS 5
+#define LOGIN_PROCESS 6
+#define USER_PROCESS 7
+#define DEAD_PROCESS 8
+
+#endif /* _ABIBITS_UTMP_DEFINES_H */
