84349b1ab28ac235e2a38e07eda97d6b470a6da3
no92 <leo@managarm.org>
2025-08-06 20:33:59 +0200

    sysdeps/linux: CFI annotate sigreturn
    This enables debuggers (gdb, lldb) to corectly traverse signal handlers
    in the backtrace correctly.

--- options/internal/include/mlibc-asm/helpers.h
+++ options/internal/include/mlibc-asm/helpers.h
@@ -10,9 +10,27 @@
 	.cfi_startproc; \
 	name:
 
+#define PROC_START_NOCFI(name) \
+	.global name; \
+	.type name, @function; \
+	name:
+
 #define PROC_END(name) \
 	.cfi_endproc; \
 	.size name, . - name
+
+#define PROC_END_NOCFI(name) \
+	.size name, . - name
+
+#define PROC_ALIAS(name, alias) \
+	.global alias; \
+	.type alias, @function; \
+	.set alias, name
+
+#define PROC_HIDDEN_ALIAS(name, alias) \
+	.hidden alias; \
+	.type alias, @function; \
+	.set alias, name
 
 #define GNU_STACK_NOTE() \
 	.section .note.GNU-stack,"",%progbits
