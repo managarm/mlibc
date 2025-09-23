62f5346e8a8cb6c96667dafd35b44ac65dd911ad
no92 <leo@managarm.org>
2025-09-24 00:48:13 +0200

    options/internal: fix `off_t` and `off64_t`
    off64_t was incorrectly typedefed to long, which breaks on 32-bit
    architectures. Because we always use 64-bit offsets, we can also define
    off_t to always be 64-bit instead of long.

--- options/internal/include/bits/off_t.h
+++ options/internal/include/bits/off_t.h
@@ -1,8 +1,9 @@
 #ifndef MLIBC_OFF_T_H
 #define MLIBC_OFF_T_H
 
-/* TODO: use something like int64_t instead? */
-typedef long off_t;
-typedef long off64_t;
+#include <bits/types.h>
+
+typedef __mlibc_int64 off_t;
+typedef __mlibc_int64 off64_t;
 
 #endif /* MLIBC_OFF_T_H */
