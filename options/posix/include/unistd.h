e65b2f3ee9b8a09d697a527c5ca0a1cc4bbe4a5c
no92 <leo@managarm.org>
2025-09-23 22:22:40 +0200

    [ABI BREAK] options/{ansi,posix}: fix incorrect function return values

--- options/posix/include/unistd.h
+++ options/posix/include/unistd.h
@@ -264,7 +264,7 @@ unsigned int alarm(unsigned int __seconds);
 int chdir(const char *__path);
 int chown(const char *__path, uid_t __uid, gid_t __gid);
 int close(int __fd);
-ssize_t confstr(int __name, char *__buf, size_t __size);
+size_t confstr(int __name, char *__buf, size_t __size);
 char *ctermid(char *__s);
 int dup(int __fd);
 int dup2(int __src_fd, int __dest_fd);
