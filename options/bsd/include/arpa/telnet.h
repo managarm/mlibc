7dc711386259ac96a870e93f8f9cce7e709f1235
Dennis Bonke <admin@dennisbonke.com>
2025-11-06 00:56:28 +0100

    options/bsd: Add basic telnet defines

---
+++ options/bsd/include/arpa/telnet.h
@@ -0,0 +1,25 @@
+#ifndef _ARPA_TELNET_H
+#define _ARPA_TELNET_H
+
+#define	IAC 255
+#define	DONT 254
+#define	DO 253
+#define	WONT 252
+#define	WILL 251
+#define	SB 250
+#define	GA 249
+#define	EL 248
+#define	EC 247
+#define	AYT 246
+#define	AO 245
+#define	IP 244
+#define	BREAK 243
+#define	DM 242
+#define	NOP 241
+#define	SE 240
+#define EOR 239
+#define	ABORT 238
+#define	SUSP 237
+#define	xEOF 236
+
+#endif /* _ARPA_TELNET_H */
