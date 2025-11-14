2be6566f5cbf7a4ea52812c250371f3a9b0af250
no92 <leo@managarm.org>
2025-11-14 14:28:46 +0100

    options/ansi: fix `stdlib.h` pollution

--- options/posix/include/bits/posix/posix_stdlib.h
+++ options/posix/include/bits/posix/posix_stdlib.h
@@ -4,6 +4,7 @@
 
 #include <bits/posix/locale_t.h>
 #include <bits/size_t.h>
+#include <mlibc-config.h>
 
 #ifdef __cplusplus
 extern "C" {
@@ -11,6 +12,7 @@ extern "C" {
 
 #ifndef __MLIBC_ABI_ONLY
 
+#if defined(_DEFAULT_SOURCE) || __MLIBC_XOPEN >= 500
 long random(void);
 double drand48(void);
 double erand48(unsigned short __s[3]);
@@ -21,14 +23,20 @@ long jrand48(unsigned short __s[3]);
 char *initstate(unsigned int __seed, char *__state, size_t __size);
 char *setstate(char *__state);
 void srandom(unsigned int __seed);
+#endif /* defined(_DEFAULT_SOURCE) || __MLIBC_XOPEN >= 500 */
 
 /* ---------------------------------------------------------------------------- */
 /* Environment. */
 /* ---------------------------------------------------------------------------- */
 
+#if defined(_DEFAULT_SOURCE) || __MLIBC_XOPEN
 int putenv(char *__string);
+#endif /* defined(_DEFAULT_SOURCE) || __MLIBC_XOPEN */
+
+#if defined(_DEFAULT_SOURCE) || __MLIBC_POSIX2001
 int setenv(const char *__name, const char *__value, int __overwrite);
 int unsetenv(const char *__name);
+#endif /* defined(_DEFAULT_SOURCE) || __MLIBC_POSIX2001 */
 
 /* ---------------------------------------------------------------------------- */
 /* Path handling. */
@@ -49,20 +57,35 @@ char *mkdtemp(char *__path);
 int mkostemp(char *__pattern, int __flags);
 #endif
 
+#if defined(_DEFAULT_SOURCE)
 int mkstemps(char *__pattern, int __suffixlen);
-int mkostemps(char *__pattern, int __suffixlen, int __flags);
+#endif /* defined(_DEFAULT_SOURCE) */
 
+#if defined(_GNU_SOURCE)
+int mkostemps(char *__pattern, int __suffixlen, int __flags);
+#endif /* defined(_GNU_SOURCE) */
+
+#if defined(_DEFAULT_SOURCE) || (defined(__MLIBC_XOPEN) && __MLIBC_XOPEN < 800) || __MLIBC_POSIX2024
 char *realpath(const char *__restrict __path, char *__restrict __out);
+#endif /* defined(_DEFAULT_SOURCE) || (defined(__MLIBC_XOPEN) && __MLIBC_XOPEN < 800) || __MLIBC_POSIX2024 */
 
 /* ---------------------------------------------------------------------------- */
 /* Pseudoterminals */
 /* ---------------------------------------------------------------------------- */
 
+#if __MLIBC_XOPEN >= 600
 int posix_openpt(int __flags);
+#endif /* __MLIBC_XOPEN >= 600 */
+
+#if __MLIBC_XOPEN >= 500
 int grantpt(int __fd);
 int unlockpt(int __fd);
 char *ptsname(int __fd);
+#endif /* __MLIBC_XOPEN >= 500 */
+
+#if defined(_GNU_SOURCE) || __MLIBC_XOPEN >= 800
 int ptsname_r(int __fd, char *__buf, size_t __len);
+#endif /* defined(_GNU_SOURCE) || __MLIBC_XOPEN >= 800 */
 
 double strtod_l(const char *__restrict__ __nptr, char ** __restrict__ __endptr, locale_t __loc);
 long double strtold_l(const char *__restrict__ __nptr, char ** __restrict__ __endptr, locale_t __loc);
@@ -70,14 +93,21 @@ float strtof_l(const char *__restrict __string, char **__restrict __end, locale_
 
 int getsubopt(char **__restrict__ __optionp, char *const *__restrict__ __tokens, char **__restrict__ __valuep);
 
-/* GNU extension */
+#if defined(_GNU_SOURCE) || __MLIBC_POSIX2024
 char *secure_getenv(const char *__name);
+#endif /* defined(_GNU_SOURCE) || __MLIBC_POSIX2024 */
+
+#if defined(_GNU_SOURCE)
 char *canonicalize_file_name(const char *__name);
+#endif /* defined(_GNU_SOURCE) */
 
-/* BSD extension */
+#if defined(_DEFAULT_SOURCE) || __MLIBC_POSIX2024
 void *reallocarray(void *__ptr, size_t __count, size_t __size);
+#endif /* defined(_DEFAULT_SOURCE) || __MLIBC_POSIX2024 */
 
+#if defined(_DEFAULT_SOURCE)
 int clearenv(void);
+#endif /* defined(_DEFAULT_SOURCE) */
 
 #endif /* !__MLIBC_ABI_ONLY */
 
