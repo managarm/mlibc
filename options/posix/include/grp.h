ad3fa777d6136c93121ff75e8a661e5b497b4cae
no92 <leo@managarm.org>
2025-11-15 01:37:27 +0100

    options/posix: fix `grp.h` pollution

--- options/posix/include/grp.h
+++ options/posix/include/grp.h
@@ -1,9 +1,10 @@
 #ifndef _GRP_H
 #define _GRP_H
 
-#include <stddef.h>
-#include <stdio.h>
+#include <mlibc-config.h>
+
 #include <abi-bits/gid_t.h>
+#include <bits/size_t.h>
 
 #ifdef __cplusplus
 extern "C" {
@@ -18,21 +19,32 @@ struct group {
 
 #ifndef __MLIBC_ABI_ONLY
 
+#if defined(_DEFAULT_SOURCE) || __MLIBC_XOPEN
 void endgrent(void);
 struct group *getgrent(void);
+void setgrent(void);
+#endif /* defined(_DEFAULT_SOURCE) || __MLIBC_XOPEN */
+
 struct group *getgrgid(gid_t __gid);
 int getgrgid_r(gid_t __gid, struct group *__grp, char *__buf, size_t __buflen, struct group **__res);
 struct group *getgrnam(const char *__name);
 int getgrnam_r(const char *__name, struct group *__grp, char *__buf, size_t __buflen, struct group **__res);
-void setgrent(void);
+
+#if defined(_GNU_SOURCE)
+#include <bits/file.h>
+
 int putgrent(const struct group *__grp, FILE *__stream);
+#endif
+
+#if defined(_DEFAULT_SOURCE)
+#include <bits/file.h>
 struct group *fgetgrent(FILE *__stream);
 
 int setgroups(size_t __size, const gid_t *__list);
 int initgroups(const char *__user, gid_t __group);
 
-/* Non standard extension */
 int getgrouplist(const char *__user, gid_t __group, gid_t *__groups, int *__ngroups);
+#endif /* defined(_DEFAULT_SOURCE) */
 
 #endif /* !__MLIBC_ABI_ONLY */
 
