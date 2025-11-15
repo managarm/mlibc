67ed5fc533970fad13f9f4aa893e2d72de1a1a9b
no92 <leo@managarm.org>
2025-11-15 01:41:48 +0100

    options/posix: fix `sys/wait.h` pollution

--- options/posix/include/sys/wait.h
+++ options/posix/include/sys/wait.h
@@ -2,6 +2,8 @@
 #ifndef _SYS_WAIT_H
 #define _SYS_WAIT_H
 
+#include <mlibc-config.h>
+
 #include <bits/posix/id_t.h>
 #include <abi-bits/pid_t.h>
 /* for siginfo_t */
@@ -11,9 +13,6 @@
 #ifdef __cplusplus
 extern "C" {
 #endif
-
-/* According to POSIX, <sys/wait.h> does not make rusage available. */
-struct rusage;
 
 /* TODO: move to own file and include in sys/types.h */
 typedef enum {
@@ -26,9 +25,15 @@ pid_t wait(int *__status);
 int waitid(idtype_t __idtype, id_t __id, siginfo_t *__siginfo, int __flags);
 pid_t waitpid(pid_t __pid, int *__status, int __flags);
 
-/* GNU extensions. */
+#if defined(_DEFAULT_SOURCE) || (__MLIBC_POSIX1 && !__MLIBC_POSIX2001)
+struct rusage;
+
 pid_t wait3(int *__status, int __options, struct rusage *__ru);
+#endif /* defined(_DEFAULT_SOURCE) || (__MLIBC_POSIX1 && !__MLIBC_POSIX2001) */
+
+#if defined(_DEFAULT_SOURCE)
 pid_t wait4(pid_t __pid, int *__status, int __options, struct rusage *__ru);
+#endif /* defined(_DEFAULT_SOURCE) */
 
 #endif /* !__MLIBC_ABI_ONLY */
 
