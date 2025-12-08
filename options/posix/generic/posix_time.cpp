faaf0a2060b69da143b351a1dc54f4552065faaf
no92 <leo@managarm.org>
2025-12-08 16:56:52 +0100

    options/posix: implement `futimes`

--- options/posix/generic/posix_time.cpp
+++ options/posix/generic/posix_time.cpp
@@ -39,9 +39,27 @@ int utimes(const char *filename, const struct timeval times[2]) {
 	return 0;
 }
 
-int futimes(int, const struct timeval[2]) {
-	__ensure(!"Not implemented");
-	__builtin_unreachable();
+int futimes(int fd, const struct timeval times[2]) {
+	MLIBC_CHECK_OR_ENOSYS(mlibc::sys_utimensat, -1);
+	struct timespec time[2];
+	if(times == nullptr) {
+		time[0].tv_sec = UTIME_NOW;
+		time[0].tv_nsec = UTIME_NOW;
+		time[1].tv_sec = UTIME_NOW;
+		time[1].tv_nsec = UTIME_NOW;
+	} else {
+		time[0].tv_sec = times[0].tv_sec;
+		time[0].tv_nsec = times[0].tv_usec * 1000;
+		time[1].tv_sec = times[1].tv_sec;
+		time[1].tv_nsec = times[1].tv_usec * 1000;
+	}
+
+	if (int e = mlibc::sys_utimensat(fd, "", time, AT_EMPTY_PATH); e) {
+		errno = e;
+		return -1;
+	}
+
+	return 0;
 }
 
 int lutimes(const char *filename, const struct timeval tv[2]) {
