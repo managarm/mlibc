559ea0b89d4fe5062e6c3b3023dab1586090fb2a
Dennis Bonke <admin@dennisbonke.com>
2026-01-11 22:07:40 +0100

    [ABI BREAK]: abis: Fix definition of fd_set

--- abis/linux/fd_set.h
+++ abis/linux/fd_set.h
@@ -1,12 +1,23 @@
 #ifndef _ABIBITS_FD_SET_H
 #define _ABIBITS_FD_SET_H
 
+#include <mlibc-config.h>
+
 #include <bits/types.h>
 
 #define FD_SETSIZE 1024
 
-typedef struct __attribute__((__aligned__(__alignof__(long)))) {
-	__mlibc_uint8 fds_bits[128];
+typedef long int __fd_mask;
+#define __NFDBITS (8 * (int) sizeof (__fd_mask))
+
+typedef struct {
+	/* libtirpc reaches into fds_bits directly, so we must keep the name and type exactly as expected.
+	 * This now matches the definition in sys/select.h from glibc. */
+#if __MLIBC_XOPEN
+	__fd_mask fds_bits[FD_SETSIZE / __NFDBITS];
+#else
+	__fd_mask __fds_bits[FD_SETSIZE / __NFDBITS];
+#endif
 } fd_set;
 
 #endif /* _ABIBITS_FD_SET_H */
