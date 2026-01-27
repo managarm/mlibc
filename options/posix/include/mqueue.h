1f31d0fb3a69732fe2b0ea7f0124ca0471ce0931
Dennis Bonke <admin@dennisbonke.com>
2026-01-27 23:48:53 +0100

    options/posix: Stub mq_receive

--- options/posix/include/mqueue.h
+++ options/posix/include/mqueue.h
@@ -2,6 +2,8 @@
 #define _MQUEUE_H
 
 #include <abi-bits/mqueue.h>
+#include <bits/size_t.h>
+#include <bits/ssize_t.h>
 
 #ifdef __cplusplus
 extern "C" {
@@ -15,6 +17,7 @@ int mq_getattr(mqd_t __mqdes, struct mq_attr *__attr);
 int mq_setattr(mqd_t __mqdes, const struct mq_attr *__restrict__ __newattr, struct mq_attr *__restrict__ __oldattr);
 int mq_unlink(const char *__name);
 mqd_t mq_open(const char *__name, int __flags, ...);
+ssize_t mq_receive(mqd_t __mqdes, char *__msg_ptr, size_t __msg_len, unsigned int *__msg_prio);
 
 #endif /* !__MLIBC_ABI_ONLY */
 
