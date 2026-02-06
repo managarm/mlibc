75815c645cfcce1dfe54ff8ec57325196f942190
Julian Scheffers <julian@scheffers.net>
2026-02-06 18:54:17 +0100

    Implement more sysdeps

--- abis/badgeros/wait.h
+++ abis/badgeros/wait.h
@@ -12,25 +12,25 @@
 
 #define __WCOREFLAG 0x80
 
-#define WEXITSTATUS(x) (((status) & 0xff00) >> 8)
-#define WTERMSIG(x) (((status) & 0xff00) >> 8)
-#define WSTOPSIG(x) (((status) & 0xff00) >> 8)
+#define WEXITSTATUS(x) (((x) & 0xff00) >> 8)
+#define WTERMSIG(x) (((x) & 0xff00) >> 8)
+#define WSTOPSIG(x) (((x) & 0xff00) >> 8)
 
-// Whether the child exited normally (by means of `SYSCALL_PROC_EXIT`).
-#define WIFEXITED(status) (((status) & 0xff) == 0)
-// Whether the child was killed by a signal.
-#define WIFSIGNALED(status) ((status) & 0x40)
-// Whether the child was suspended by a signal.
-#define WIFSTOPPED(status) ((status) & 0x20)
-// Whether the child was resumed by `SIGCONT`.
-#define WIFCONTINUED(status) ((status) & 0x10)
+/* Whether the child exited normally (by means of `SYSCALL_PROC_EXIT`). */
+#define WIFEXITED(x) (((x) & 0xff) == 0)
+/* Whether the child was killed by a signal. */
+#define WIFSIGNALED(x) ((x) & 0x40)
+/* Whether the child was suspended by a signal. */
+#define WIFSTOPPED(x) ((x) & 0x20)
+/* Whether the child was resumed by `SIGCONT`. */
+#define WIFCONTINUED(x) ((x) & 0x10)
 
 #if defined(_DEFAULT_SOURCE)
 #define WCOREFLAG __WCOREFLAG
 #endif
 
 #if defined(_DEFAULT_SOURCE) || __MLIBC_POSIX2024
-// Whether the child dumped core.
+/* Whether the child dumped core. */
 #define WCOREDUMP(x) ((x) & __WCOREFLAG)
 #endif /* defined(_DEFAULT_SOURCE) || __MLIBC_POSIX2024 */
 
