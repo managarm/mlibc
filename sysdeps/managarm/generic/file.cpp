cc6c3c169fc98f6db484856ab1ec25724e6cf547
Dennis Bonke <admin@dennisbonke.com>
2026-02-13 18:16:46 +0100

    sysdeps/managarm: Make sys_name_to_handle_at return EOPNOTSUPP

--- sysdeps/managarm/generic/file.cpp
+++ sysdeps/managarm/generic/file.cpp
@@ -3133,8 +3133,8 @@ int sys_lgetxattr(const char *, const char *, void *, size_t, ssize_t *) { retur
 
 int sys_setxattr(const char *, const char *, const void *, size_t, int) { return ENOSYS; }
 
-// We don't implement name_to_handle_at
-int sys_name_to_handle_at(int, const char *, struct file_handle *, int *, int) { return ENOSYS; }
+// We don't implement name_to_handle_at, EOPNOTSUPP will cause systemd to fall through to backup options
+int sys_name_to_handle_at(int, const char *, struct file_handle *, int *, int) { return EOPNOTSUPP; }
 
 int sys_setgroups(size_t size, const gid_t *list) {
 	SignalGuard sguard;
