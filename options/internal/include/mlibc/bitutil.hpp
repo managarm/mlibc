48daa200ff5ccc9862715db374600a78758cafd4
NetBSD User <netbsduser@users.noreply.github.com>
2026-02-22 14:12:44 +0000

    options/ansi: don't byteswap on big-endian

--- options/internal/include/mlibc/bitutil.hpp
+++ options/internal/include/mlibc/bitutil.hpp
@@ -13,6 +13,13 @@ struct bit_util<uint64_t> {
 	static uint64_t byteswap(uint64_t x) {
 		return __builtin_bswap64(x);
 	}
+	static uint64_t be_to_host(uint64_t x) {
+#if __BYTE_ORDER__ == __ORDER_LITTLE_ENDIAN__
+		return byteswap(x);
+#else
+		return x;
+#endif
+	}
 };
 
 template<>
@@ -20,12 +27,26 @@ struct bit_util<uint32_t> {
 	static uint32_t byteswap(uint32_t x) {
 		return __builtin_bswap32(x);
 	}
+	static uint32_t be_to_host(uint32_t x) {
+#if __BYTE_ORDER__ == __ORDER_LITTLE_ENDIAN__
+		return byteswap(x);
+#else
+		return x;
+#endif
+	}
 };
 
 template<>
 struct bit_util<uint16_t> {
 	static uint16_t byteswap(uint16_t x) {
 		return __builtin_bswap16(x);
+	}
+	static uint16_t be_to_host(uint16_t x) {
+#if __BYTE_ORDER__ == __ORDER_LITTLE_ENDIAN__
+		return byteswap(x);
+#else
+		return x;
+#endif
 	}
 };
 
