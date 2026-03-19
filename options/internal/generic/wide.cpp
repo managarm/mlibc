9a6ee86424593bec67271cdfcaa4ca263d6c2f60
no92 <leo@managarm.org>
2026-03-19 13:14:35 +0100

    options/internal: fix incorrect wctrans returns

--- options/internal/generic/wide.cpp
+++ options/internal/generic/wide.cpp
@@ -6,20 +6,20 @@ wctrans_t find_wctrans(frg::string_view property, mlibc::localeinfo *l) {
 	auto names_map = l->ctype.map_names();
 	size_t index = 0;
 
-	while (names_map.size() && !names_map.starts_with("")) {
+	while (names_map.size() && names_map[0] != '\0') {
 		size_t end = names_map.find_first('\0');
 		size_t name_len = (end != size_t(-1)) ? end : names_map.size();
 
 		auto name = names_map.sub_string(0, name_len);
 		if (name == property)
-			break;
+			return l->ctype.map_offset() + index;
 
 		if (end != size_t(-1))
 			names_map = names_map.sub_string(end + 1, names_map.size() - (end + 1));
 		index++;
 	}
 
-	return l->ctype.map_offset() + index;
+	return 0;
 }
 
 } // namespace mlibc
