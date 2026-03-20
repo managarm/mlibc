db04261e762e9c6285876429abefe49864afb661
no92 <leo@managarm.org>
2026-03-20 14:27:27 +0100

    tests/ansi: test output pointers for wcs(n)rtombs

--- tests/ansi/wcsnrtombs.c
+++ tests/ansi/wcsnrtombs.c
@@ -1,21 +1,52 @@
 #include <assert.h>
-#include <wchar.h>
-#include <stdlib.h>
 #include <errno.h>
 #include <locale.h>
+#include <stdlib.h>
+#include <string.h>
+#include <wchar.h>
 
 int main() {
 	char *lang = setlocale(LC_ALL, "en_US.utf8");
 	assert(lang);
 
-	const wchar_t *p = L"abcde";
-	assert(wcsnrtombs(NULL, &p, 3, 343245234, NULL) == 3);
+	const wchar_t *wcs = L"abcde";
+	const wchar_t *p = wcs;
+	size_t ret = wcsnrtombs(NULL, &p, 3, 343245234, NULL);
+	assert(ret == 3);
 
-	p = L"abcd\u03f4"; // abcdTHETA
-	assert(wcsnrtombs(NULL, &p, 5, 343245234, NULL) == 6);
+	wcs = L"abcd\u03f4"; // abcdTHETA
+	p = wcs;
+	ret = wcsnrtombs(NULL, &p, 5, 343245234, NULL);
+	assert(ret == 6);
 
-	p = L"ab\xffffffff";
-	assert(wcsnrtombs(NULL, &p, 3, 343245234, NULL) == (size_t)-1);
+	wcs = L"ab\xffffffff";
+	p = wcs;
+	ret = wcsnrtombs(NULL, &p, 3, 343245234, NULL);
+	assert(ret == (size_t)-1);
 	assert(errno == EILSEQ);
+
+	mbstate_t ps = {0};
+	char mbs[6] = "";
+	wcs = L"abcde";
+	p = wcs;
+	ret = wcsnrtombs(mbs, &p, 6, 4, &ps);
+	assert(ret == 4);
+	assert(!strcmp(mbs, "abcd"));
+	assert(p == &wcs[4]);
+
+	memset(&ps, 0, sizeof(ps));
+	p = wcs;
+	ret = wcsnrtombs(mbs, &p, 6, 5, &ps);
+	assert(ret == 5);
+	assert(!strcmp(mbs, "abcde"));
+	assert(p == &wcs[5]);
+
+	memset(&ps, 0, sizeof(ps));
+	p = wcs;
+	ret = wcsnrtombs(mbs, &p, 6, 6, &ps);
+	assert(ret == 5);
+	assert(!strcmp(mbs, "abcde"));
+	assert(!p);
+
 	return 0;
 }
--- tests/ansi/wcsrtombs.c
+++ tests/ansi/wcsrtombs.c
@@ -1,18 +1,36 @@
-#include <stdlib.h>
-#include <wchar.h>
 #include <assert.h>
-#include <string.h>
 #include <errno.h>
+#include <stdlib.h>
+#include <string.h>
+#include <wchar.h>
 
 int main() {
-	const wchar_t str[] = L"wcsrtombs";
+	const wchar_t *str = L"wcsrtombs";
 	const wchar_t *p = str;
-	int ret = wcsrtombs(NULL, &p, 343245234, NULL);
+	size_t ret = wcsrtombs(NULL, &p, 343245234, NULL);
 	assert(ret == 9);
 
-	p = L"\xffffffff";
+	str = L"\xffffffff";
+	p = str;
 	ret = wcsrtombs(NULL, &p, 343245234, NULL);
-	assert(ret == -1);
+	assert(ret == (size_t) -1);
 	assert(errno == EILSEQ);
+
+	mbstate_t ps = {0};
+	char mbs[10] = "";
+	str = L"wcsrtombs";
+	p = str;
+	ret = wcsrtombs(mbs, &p, 3, &ps);
+	assert(ret == 3);
+	assert(!strncmp(mbs, "wcs", 3));
+	assert(p == &str[3]);
+
+	memset(&ps, 0, sizeof(ps));
+	p = str;
+	ret = wcsrtombs(mbs, &p, 10, &ps);
+	assert(ret == 9);
+	assert(!strcmp(mbs, "wcsrtombs"));
+	assert(!p);
+
 	return 0;
 }
