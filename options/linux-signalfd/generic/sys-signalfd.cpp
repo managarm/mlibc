3f5d5d3cc77b99fe6eb348b9eddb5cd45968bdc1
no92 <leo@managarm.org>
2026-04-04 13:12:58 +0200

    options/linux-signalfd: add new signalfd_siginfo members

--- options/linux-signalfd/generic/sys-signalfd.cpp
+++ options/linux-signalfd/generic/sys-signalfd.cpp
@@ -6,6 +6,8 @@
 #include <mlibc/all-sysdeps.hpp>
 #include <mlibc/debug.hpp>
 
+static_assert(sizeof(signalfd_siginfo) == 128, "signalfd_siginfo must be exactly 128 bytes!");
+
 int signalfd(int fd, const sigset_t *mask, int flags) {
 	if(int e = mlibc::sysdep_or_enosys<SignalfdCreate>(mask, flags, &fd); e) {
 		errno = e;
